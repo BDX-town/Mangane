e33b621fb90a33427fe7316492f8a44ab4c39653
Alex Gleason <alex@alexgleason.me>
2020-04-01 14:40:14 -0500

    Clean up Patron code

--- app/gabsocial/actions/patron.js
+++ app/gabsocial/actions/patron.js
@@ -1,11 +1,7 @@
-import api, { getLinks } from '../api';
-import openDB from '../storage/db';
-import { me } from 'gabsocial/initial_state';
+import api from '../api';
 
-export const PATRON_FUNDING_FETCH_REQUEST = 'PATRON_FUNDING_FETCH_REQUEST';
-export const PATRON_FUNDING_FETCH_SUCCESS = 'PATRON_FUNDING_FETCH_SUCCESS';
-export const PATRON_FUNDING_FETCH_FAIL    = 'PATRON_FUNDING_FETCH_FAIL';
 export const PATRON_FUNDING_IMPORT        = 'PATRON_FUNDING_IMPORT';
+export const PATRON_FUNDING_FETCH_FAIL    = 'PATRON_FUNDING_FETCH_FAIL';
 
 export function fetchFunding() {
   return (dispatch, getState) => {
@@ -25,18 +21,6 @@ export function importFetchedFunding(funding) {
     funding
   };
 }
-
-export function fetchFundingRequest() {
-  return {
-    type: PATRON_FUNDING_FETCH_REQUEST,
-  };
-};
-
-export function fetchAccountSuccess() {
-  return {
-    type: PATRON_FUNDING_FETCH_SUCCESS,
-  };
-};
 
 export function fetchFundingFail(error) {
   return {
