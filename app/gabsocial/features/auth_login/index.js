42e584ca72f4cc7664f0a5897923d4e41efa8aba
Alex Gleason <alex@alexgleason.me>
2020-04-06 16:20:03 -0500

    Disable auth form during requests

--- app/gabsocial/features/auth_login/index.js
+++ app/gabsocial/features/auth_login/index.js
@@ -4,12 +4,18 @@ import ImmutablePureComponent from 'react-immutable-pure-component';
 import { createAuthApp, logIn } from 'gabsocial/actions/auth';
 import { Redirect } from 'react-router-dom';
 import { fetchMe } from 'gabsocial/actions/me';
+import { LoadingBar } from 'react-redux-loading-bar';
 
 const mapStateToProps = (state, props) => ({
   me: state.get('me'),
 });
 
 class LoginForm extends ImmutablePureComponent {
+
+  constructor(props) {
+    super(props);
+    this.state = {isLoading: false};
+  }
 
   componentWillMount() {
     this.props.dispatch(createAuthApp());
@@ -25,21 +31,27 @@ class LoginForm extends ImmutablePureComponent {
     const { dispatch } = this.props;
     const { username, password } = this.getFormData(event.target);
     dispatch(logIn(username, password)).then(() => {
-      dispatch(fetchMe());
+      return dispatch(fetchMe());
+    }).catch(() => {
+      // TODO: Handle bad request
+      this.setState({isLoading: false});
     });
+    this.setState({isLoading: true});
     event.preventDefault();
   }
 
   render() {
     const { me } = this.props;
-
     if (me) return <Redirect to="/home" />;
 
     return (
       <form onSubmit={this.handleSubmit}>
-        <input name='username' placeholder='me@example.com' />
-        <input name='password' type='password' placeholder='Password' />
-        <input type='submit' value='Login' />
+        <LoadingBar loading={this.state.isLoading ? 1 : 0} className='loading-bar' />
+        <fieldset disabled={this.state.isLoading}>
+          <input name='username' placeholder='me@example.com' />
+          <input name='password' type='password' placeholder='Password' />
+          <input type='submit' value='Login' />
+        </fieldset>
       </form>
     )
   }
