ab0208943389859c4938e659243a4d0217a9212e
Alex Gleason <alex@alexgleason.me>
2020-04-11 21:04:55 -0500

    Redirect LoginPage after logging in

--- app/gabsocial/features/auth_login/components/login_page.js
+++ app/gabsocial/features/auth_login/components/login_page.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux'
+import { Redirect } from 'react-router-dom';
 import ImmutablePureComponent from 'react-immutable-pure-component';
 import LoginForm from './login_form';
 
-export default @connect()
+const mapStateToProps = state => ({
+  me: state.get('me'),
+});
+
+export default @connect(mapStateToProps)
 class LoginPage extends ImmutablePureComponent {
   render() {
+    const { me } = this.props;
+    if (me) return <Redirect to='/' />;
+
     return <LoginForm />
   }
 }
