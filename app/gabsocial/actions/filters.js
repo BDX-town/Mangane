32a5a3159155c9e96ca8ffc8fc8ee6cdf3cb9993
Alex Gleason <alex@alexgleason.me>
2020-04-25 19:04:47 -0500

    Add createFilter function

--- app/gabsocial/actions/filters.js
+++ app/gabsocial/actions/filters.js
@@ -4,6 +4,10 @@ export const FILTERS_FETCH_REQUEST = 'FILTERS_FETCH_REQUEST';
 export const FILTERS_FETCH_SUCCESS = 'FILTERS_FETCH_SUCCESS';
 export const FILTERS_FETCH_FAIL    = 'FILTERS_FETCH_FAIL';
 
+export const FILTERS_CREATE_REQUEST = 'FILTERS_CREATE_REQUEST';
+export const FILTERS_CREATE_SUCCESS = 'FILTERS_CREATE_SUCCESS';
+export const FILTERS_CREATE_FAIL    = 'FILTERS_CREATE_FAIL';
+
 export const fetchFilters = () => (dispatch, getState) => {
   if (!getState().get('me')) return;
 
@@ -26,3 +30,14 @@ export const fetchFilters = () => (dispatch, getState) => {
       skipAlert: true,
     }));
 };
+
+export function createFilter(params) {
+  return (dispatch, getState) => {
+    dispatch({ type: FILTERS_CREATE_REQUEST });
+    return api(getState).post('/api/v1/filters', params).then(response => {
+      dispatch({ type: FILTERS_CREATE_SUCCESS, filter: response.data });
+    }).catch(error => {
+      dispatch({ type: FILTERS_CREATE_FAIL, error });
+    });
+  };
+}
