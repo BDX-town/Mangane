05db2fc35f84c86177acf4d8e378023f47967ced
Alex Gleason <alex@alexgleason.me>
2020-04-27 21:12:59 -0500

    Fix eslint unused variable

--- app/gabsocial/actions/notifications.js
+++ app/gabsocial/actions/notifications.js
@@ -13,7 +13,7 @@ import { defineMessages } from 'react-intl';
 import { List as ImmutableList } from 'immutable';
 import { unescapeHTML } from '../utils/html';
 import { getFilters, regexFromFilters } from '../selectors';
-import { fetchMarkers, saveMarkers } from './markers';
+import { fetchMarkers } from './markers';
 
 export const NOTIFICATIONS_UPDATE      = 'NOTIFICATIONS_UPDATE';
 export const NOTIFICATIONS_UPDATE_NOOP = 'NOTIFICATIONS_UPDATE_NOOP';
