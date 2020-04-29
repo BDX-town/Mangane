238eed4c1a8d44c416ac795ec7bdcae56c097e32
Alex Gleason <alex@alexgleason.me>
2020-04-29 14:06:26 -0500

    Allow API auth with app or user token

--- app/gabsocial/api.js
+++ app/gabsocial/api.js
@@ -25,13 +25,15 @@ function setCSRFHeader() {
 
 ready(setCSRFHeader);
 
-export default getState => {
-  const user_token = getState().getIn(['auth', 'user', 'access_token']);
-  const app_token  = getState().getIn(['auth', 'app', 'access_token']);
-  const access_token = user_token || app_token;
+const getToken = (getState, authType) =>
+  getState().getIn(['auth', authType, 'access_token']);
+
+export default (getState, authType = 'user') => {
+  const accessToken = getToken(getState, authType);
+
   return axios.create({
-    headers: Object.assign(csrfHeader, access_token ? {
-      'Authorization': `Bearer ${access_token}`,
+    headers: Object.assign(csrfHeader, accessToken ? {
+      'Authorization': `Bearer ${accessToken}`,
     } : {}),
 
     transformResponse: [function(data) {
