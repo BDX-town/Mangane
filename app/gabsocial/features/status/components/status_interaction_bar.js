c8f89ce8c1542d0dd9c503e7429a78c0b436a79e
Alex Gleason <alex@alexgleason.me>
2020-05-20 13:11:44 -0500

    Style the StatusInteractionBar

--- app/gabsocial/features/status/components/status_interaction_bar.js
+++ app/gabsocial/features/status/components/status_interaction_bar.js
@@ -9,22 +9,37 @@ export class StatusInteractionBar extends React.Component {
     status: ImmutablePropTypes.map,
   }
 
-  render() {
+  getNormalizedReacts = () => {
     const { status } = this.props;
     const emojiReacts = status.getIn(['pleroma', 'emoji_reactions']);
     const favouritesCount = status.get('favourites_count');
+    return reduceEmoji(emojiReacts, favouritesCount).reverse();
+  }
+
+  render() {
+    const emojiReacts = this.getNormalizedReacts();
+    const count = emojiReacts.reduce((acc, cur) => (
+      acc + cur.get('count')
+    ), 0);
+
+    if (count < 1) return null;
 
     return (
-      <div className='emoji-reacts'>
-        {reduceEmoji(emojiReacts, favouritesCount).map((e, i) => (
-          <span className='emoji-react' key={i}>
-            <span
-              className='emoji-react--emoji'
-              dangerouslySetInnerHTML={{ __html: emojify(e.get('name')) }}
-            />
-            <span className='emoji-react--count'>{e.get('count')}</span>
-          </span>
-        ))}
+      <div className='emoji-reacts-container'>
+        <div className='emoji-reacts'>
+          {emojiReacts.map((e, i) => (
+            <span className='emoji-react' key={i}>
+              <span
+                className='emoji-react__emoji'
+                dangerouslySetInnerHTML={{ __html: emojify(e.get('name')) }}
+              />
+              <span className='emoji-react__count'>{e.get('count')}</span>
+            </span>
+          ))}
+        </div>
+        <div className='emoji-reacts__count'>
+          {count}
+        </div>
       </div>
     );
   }
