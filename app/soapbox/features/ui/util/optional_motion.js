c5886508ebdaf2be2f9215f3e84e8fbed3fa43dc
Alex Gleason <alex@alexgleason.me>
2020-05-28 20:50:53 -0500

    Pull reduceMotion from Redux store

--- app/soapbox/features/ui/util/optional_motion.js
+++ app/soapbox/features/ui/util/optional_motion.js
@@ -1,5 +1,20 @@
-import { reduceMotion } from '../../../initial_state';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import ReducedMotion from './reduced_motion';
 import Motion from 'react-motion/lib/Motion';
+import { getSettings } from 'soapbox/actions/settings';
 
-export default reduceMotion ? ReducedMotion : Motion;
+const mapStateToProps = state => ({
+  reduceMotion: getSettings(state).get('reduceMotion'),
+});
+
+const OptionalMotion = props => (
+  props.reduceMotion ? <ReducedMotion {...props} /> : <Motion {...props} />
+);
+
+OptionalMotion.propTypes = {
+  reduceMotion: PropTypes.bool,
+};
+
+export default connect(mapStateToProps)(OptionalMotion);
