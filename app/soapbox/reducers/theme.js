04911838770e993e2fb5de66b8f1063f2b1b1318
Alex Gleason <alex@alexgleason.me>
2020-05-30 21:48:37 -0500

    Replace $highlight-text-color

--- app/soapbox/reducers/theme.js
+++ app/soapbox/reducers/theme.js
@@ -2,14 +2,21 @@ import {
   SET_THEME,
 } from '../actions/theme';
 import { Map as ImmutableMap } from 'immutable';
-import { brightness, hue } from 'chromatism';
+import { brightness, hue, convert } from 'chromatism';
 
 const initialState = ImmutableMap();
+
+const cssrgba = (color, a) => {
+  const { r, g, b } = convert(color).rgb;
+  return `rgba(${[r, g, b, a].join(',')})`;
+};
 
 const populate = themeData => {
   const { 'brand-color': brandColor } = themeData.toObject();
   return ImmutableMap({
     'nav-ui-highlight-color': brightness(10, hue(-3, brandColor).hex).hex,
+    'brand-color-faint': cssrgba(brandColor, 0.1),
+    'highlight-text-color': brandColor,
   }).merge(themeData);
 };
 
