a2c9aeb8dd2f8e3a9c56590a10d44fe25e612cf5
Alex Gleason <alex@alexgleason.me>
2020-05-31 12:01:21 -0500

    Conditionally fetch /nodeinfo/2.1.json for Pleroma < 2.1, #137

--- app/soapbox/reducers/instance.js
+++ app/soapbox/reducers/instance.js
@@ -1,5 +1,21 @@
-import { INSTANCE_IMPORT } from '../actions/instance';
+import {
+  INSTANCE_IMPORT,
+  NODEINFO_IMPORT,
+} from '../actions/instance';
 import { Map as ImmutableMap, fromJS } from 'immutable';
+
+const nodeinfoToInstance = nodeinfo => {
+  // Match Pleroma's develop branch
+  return ImmutableMap({
+    pleroma: ImmutableMap({
+      metadata: ImmutableMap({
+        account_activation_required: nodeinfo.getIn(['metadata', 'accountActivationRequired']),
+        features: nodeinfo.getIn(['metadata', 'features']),
+        federation: nodeinfo.getIn(['metadata', 'federation']),
+      }),
+    }),
+  });
+};
 
 // Set Mastodon defaults, overridden by Pleroma servers
 const initialState = ImmutableMap({
@@ -16,6 +32,8 @@ export default function instance(state = initialState, action) {
   switch(action.type) {
   case INSTANCE_IMPORT:
     return initialState.merge(fromJS(action.instance));
+  case NODEINFO_IMPORT:
+    return nodeinfoToInstance(fromJS(action.nodeinfo)).merge(state);
   default:
     return state;
   }
