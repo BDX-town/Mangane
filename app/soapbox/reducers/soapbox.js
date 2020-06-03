3e2b282cb8e85e1254588de1d6208609021d687a
Alex Gleason <alex@alexgleason.me>
2020-06-03 14:47:49 -0500

    Postpone setting the brand color until soapbox.json succeeds or fails Fixes color flickering during load

--- app/soapbox/reducers/soapbox.js
+++ app/soapbox/reducers/soapbox.js
@@ -1,12 +1,21 @@
-import { SOAPBOX_CONFIG_REQUEST_SUCCESS } from '../actions/soapbox';
+import {
+  SOAPBOX_CONFIG_REQUEST_SUCCESS,
+  SOAPBOX_CONFIG_REQUEST_FAIL,
+} from '../actions/soapbox';
 import { Map as ImmutableMap, fromJS } from 'immutable';
 
 const initialState = ImmutableMap();
 
+const defaultState = ImmutableMap({
+  brandColor: '#0482d8', // Azure
+});
+
 export default function soapbox(state = initialState, action) {
   switch(action.type) {
   case SOAPBOX_CONFIG_REQUEST_SUCCESS:
-    return ImmutableMap(fromJS(action.soapboxConfig));
+    return defaultState.merge(ImmutableMap(fromJS(action.soapboxConfig)));
+  case SOAPBOX_CONFIG_REQUEST_FAIL:
+    return defaultState;
   default:
     return state;
   }
