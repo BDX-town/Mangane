7d58694cce04ce9f3372d18f713ac959a5eef10f
Alex Gleason <alex@alexgleason.me>
2020-06-03 11:23:21 -0500

    Set Azure as default brandColor (can still be set blank with null)

--- app/soapbox/reducers/soapbox.js
+++ app/soapbox/reducers/soapbox.js
@@ -1,12 +1,14 @@
 import { SOAPBOX_CONFIG_IMPORT } from '../actions/soapbox';
 import { Map as ImmutableMap, fromJS } from 'immutable';
 
-const initialState = ImmutableMap();
+const initialState = ImmutableMap({
+  brandColor: '#0482d8', // Azure
+});
 
 export default function soapbox(state = initialState, action) {
   switch(action.type) {
   case SOAPBOX_CONFIG_IMPORT:
-    return ImmutableMap(fromJS(action.soapboxConfig));
+    return initialState.merge(ImmutableMap(fromJS(action.soapboxConfig)));
   default:
     return state;
   }
