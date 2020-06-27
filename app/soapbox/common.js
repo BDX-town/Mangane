0c66cf43e1be08bacfb6862fc4386543d152710d
Sean King <seanking2919@protonmail.com>
2020-06-26 22:24:23 -0600

    Switch to Fork Awesome

--- app/soapbox/common.js
+++ app/soapbox/common.js
@@ -3,7 +3,7 @@
 import Rails from 'rails-ujs';
 
 export function start() {
-  require('font-awesome/css/font-awesome.css');
+  require('fork-awesome/css/fork-awesome.css');
   require.context('../images/', true);
 
   try {
