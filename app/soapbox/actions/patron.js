2d194d75aa6d8259f7881fb456dc926d063efa77
Alex Gleason <alex@alexgleason.me>
2020-07-01 15:08:18 -0500

    Patron: Expect proxied endpoints

--- app/soapbox/actions/patron.js
+++ app/soapbox/actions/patron.js
@@ -1,12 +1,11 @@
-import axios from 'axios';
+import api from '../api';
 
 export const PATRON_FUNDING_IMPORT        = 'PATRON_FUNDING_IMPORT';
 export const PATRON_FUNDING_FETCH_FAIL    = 'PATRON_FUNDING_FETCH_FAIL';
 
 export function fetchFunding() {
   return (dispatch, getState) => {
-    const baseUrl = getState().getIn(['soapbox', 'extensions', 'patron', 'baseUrl']);
-    axios.get(`${baseUrl}/api/patron/v1/instance`).then(response => {
+    api(getState).get('/api/patron/v1/instance').then(response => {
       dispatch(importFetchedFunding(response.data));
     }).catch(error => {
       dispatch(fetchFundingFail(error));
