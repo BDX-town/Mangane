7da412d8e5a6e3a0b35ed118f00bc3203873cc67
crockwave <curtis.rock@wirelessmessaging.com>
2020-07-04 11:38:50 -0500

    fixed linter errors

--- app/soapbox/reducers/__tests__/compose-test.js
+++ app/soapbox/reducers/__tests__/compose-test.js
@@ -306,14 +306,14 @@ describe('compose reducer', () => {
         description: null,
         id: '1375732379',
         pleroma: {
-          mime_type: 'image/jpeg'
+          mime_type: 'image/jpeg',
         },
         preview_url: 'https://media.gleasonator.com/media_attachments/files/000/853/856/original/7035d67937053e1d.jpg',
         remote_url: 'https://media.gleasonator.com/media_attachments/files/000/853/856/original/7035d67937053e1d.jpg',
         text_url: 'https://media.gleasonator.com/media_attachments/files/000/853/856/original/7035d67937053e1d.jpg',
         type: 'image',
-        url: 'https://media.gleasonator.com/media_attachments/files/000/853/856/original/7035d67937053e1d.jpg'
-      }
+        url: 'https://media.gleasonator.com/media_attachments/files/000/853/856/original/7035d67937053e1d.jpg',
+      },
     ];
     const action = {
       type: actions.COMPOSE_UPLOAD_SUCCESS,
@@ -372,103 +372,103 @@ describe('compose reducer', () => {
     });
   });
 
-//   it('should handle COMPOSE_MENTION', () => {
-//     const state = ImmutableMap({});
-//     const account = {
-//   '9w1HhmenIAKBHJiUs4': {
-//     header_static: 'https://media.gleasonator.com/accounts/headers/000/000/001/original/9d0e4dbf1c9dbc8f.png',
-//     display_name_html: 'Alex Gleason',
-//     bot: false,
-//     display_name: 'Alex Gleason',
-//     created_at: '2020-06-12T21:47:28.000Z',
-//     locked: false,
-//     emojis: [],
-//     header: 'https://media.gleasonator.com/accounts/headers/000/000/001/original/9d0e4dbf1c9dbc8f.png',
-//     url: 'https://gleasonator.com/users/alex',
-//     note: 'Fediverse developer. I come in peace. <a class="hashtag" data-tag="vegan" href="https://gleasonator.com/tag/vegan">#vegan</a> <a class="hashtag" data-tag="freeculture" href="https://gleasonator.com/tag/freeculture">#freeculture</a> <a class="hashtag" data-tag="atheist" href="https://gleasonator.com/tag/atheist">#atheist</a> <a class="hashtag" data-tag="antiporn" href="https://gleasonator.com/tag/antiporn">#antiporn</a> <a class="hashtag" data-tag="gendercritical" href="https://gleasonator.com/tag/gendercritical">#gendercritical</a>. Boosts ≠ endorsements.',
-//     acct: 'alex@gleasonator.com',
-//     avatar_static: 'https://media.gleasonator.com/accounts/avatars/000/000/001/original/1a630e4c4c64c948.jpg',
-//     username: 'alex',
-//     avatar: 'https://media.gleasonator.com/accounts/avatars/000/000/001/original/1a630e4c4c64c948.jpg',
-//     fields: [
-//       {
-//         name: 'Website',
-//         value: '<a href="https://alexgleason.me" rel="ugc">https://alexgleason.me</a>',
-//         name_emojified: 'Website',
-//         value_emojified: '<a href="https://alexgleason.me" rel="ugc">https://alexgleason.me</a>',
-//         value_plain: 'https://alexgleason.me'
-//       },
-//       {
-//         name: 'Pleroma+Soapbox',
-//         value: '<a href="https://soapbox.pub" rel="ugc">https://soapbox.pub</a>',
-//         name_emojified: 'Pleroma+Soapbox',
-//         value_emojified: '<a href="https://soapbox.pub" rel="ugc">https://soapbox.pub</a>',
-//         value_plain: 'https://soapbox.pub'
-//       },
-//       {
-//         name: 'Email',
-//         value: 'alex@alexgleason.me',
-//         name_emojified: 'Email',
-//         value_emojified: 'alex@alexgleason.me',
-//         value_plain: 'alex@alexgleason.me'
-//       },
-//       {
-//         name: 'Gender identity',
-//         value: 'Soyboy',
-//         name_emojified: 'Gender identity',
-//         value_emojified: 'Soyboy',
-//         value_plain: 'Soyboy'
-//       }
-//     ],
-//     pleroma: {
-//       hide_follows: false,
-//       hide_followers_count: false,
-//       background_image: null,
-//       confirmation_pending: false,
-//       is_moderator: false,
-//       hide_follows_count: false,
-//       hide_followers: false,
-//       relationship: {
-//         showing_reblogs: true,
-//         followed_by: false,
-//         subscribing: false,
-//         blocked_by: false,
-//         requested: false,
-//         domain_blocking: false,
-//         following: false,
-//         endorsed: false,
-//         blocking: false,
-//         muting: false,
-//         id: '9w1HhmenIAKBHJiUs4',
-//         muting_notifications: false
-//       },
-//       tags: [],
-//       hide_favorites: true,
-//       is_admin: false,
-//       skip_thread_containment: false
-//     },
-//     source: {
-//       fields: [],
-//       note: 'Fediverse developer. I come in peace. #vegan #freeculture #atheist #antiporn #gendercritical. Boosts ≠ endorsements.',
-//       pleroma: {
-//         actor_type: 'Person',
-//         discoverable: false
-//       },
-//       sensitive: false
-//     },
-//     id: '9w1HhmenIAKBHJiUs4',
-//     note_emojified: 'Fediverse developer. I come in peace. <a class="hashtag" data-tag="vegan" href="https://gleasonator.com/tag/vegan">#vegan</a> <a class="hashtag" data-tag="freeculture" href="https://gleasonator.com/tag/freeculture">#freeculture</a> <a class="hashtag" data-tag="atheist" href="https://gleasonator.com/tag/atheist">#atheist</a> <a class="hashtag" data-tag="antiporn" href="https://gleasonator.com/tag/antiporn">#antiporn</a> <a class="hashtag" data-tag="gendercritical" href="https://gleasonator.com/tag/gendercritical">#gendercritical</a>. Boosts ≠ endorsements.'
-//   }
-// };
-//     const action = {
-//       type: actions.COMPOSE_MENTION,
-//       account: account,
-//     };
-//     expect(reducer(state, action).toJS()).toMatchObject({
-//       text: '@alex@gleasonator.com',
-//       caretPosition: null,
-//     });
-//   });
+  //   it('should handle COMPOSE_MENTION', () => {
+  //     const state = ImmutableMap({});
+  //     const account = {
+  //   '9w1HhmenIAKBHJiUs4': {
+  //     header_static: 'https://media.gleasonator.com/accounts/headers/000/000/001/original/9d0e4dbf1c9dbc8f.png',
+  //     display_name_html: 'Alex Gleason',
+  //     bot: false,
+  //     display_name: 'Alex Gleason',
+  //     created_at: '2020-06-12T21:47:28.000Z',
+  //     locked: false,
+  //     emojis: [],
+  //     header: 'https://media.gleasonator.com/accounts/headers/000/000/001/original/9d0e4dbf1c9dbc8f.png',
+  //     url: 'https://gleasonator.com/users/alex',
+  //     note: 'Fediverse developer. I come in peace. <a class="hashtag" data-tag="vegan" href="https://gleasonator.com/tag/vegan">#vegan</a> <a class="hashtag" data-tag="freeculture" href="https://gleasonator.com/tag/freeculture">#freeculture</a> <a class="hashtag" data-tag="atheist" href="https://gleasonator.com/tag/atheist">#atheist</a> <a class="hashtag" data-tag="antiporn" href="https://gleasonator.com/tag/antiporn">#antiporn</a> <a class="hashtag" data-tag="gendercritical" href="https://gleasonator.com/tag/gendercritical">#gendercritical</a>. Boosts ≠ endorsements.',
+  //     acct: 'alex@gleasonator.com',
+  //     avatar_static: 'https://media.gleasonator.com/accounts/avatars/000/000/001/original/1a630e4c4c64c948.jpg',
+  //     username: 'alex',
+  //     avatar: 'https://media.gleasonator.com/accounts/avatars/000/000/001/original/1a630e4c4c64c948.jpg',
+  //     fields: [
+  //       {
+  //         name: 'Website',
+  //         value: '<a href="https://alexgleason.me" rel="ugc">https://alexgleason.me</a>',
+  //         name_emojified: 'Website',
+  //         value_emojified: '<a href="https://alexgleason.me" rel="ugc">https://alexgleason.me</a>',
+  //         value_plain: 'https://alexgleason.me'
+  //       },
+  //       {
+  //         name: 'Pleroma+Soapbox',
+  //         value: '<a href="https://soapbox.pub" rel="ugc">https://soapbox.pub</a>',
+  //         name_emojified: 'Pleroma+Soapbox',
+  //         value_emojified: '<a href="https://soapbox.pub" rel="ugc">https://soapbox.pub</a>',
+  //         value_plain: 'https://soapbox.pub'
+  //       },
+  //       {
+  //         name: 'Email',
+  //         value: 'alex@alexgleason.me',
+  //         name_emojified: 'Email',
+  //         value_emojified: 'alex@alexgleason.me',
+  //         value_plain: 'alex@alexgleason.me'
+  //       },
+  //       {
+  //         name: 'Gender identity',
+  //         value: 'Soyboy',
+  //         name_emojified: 'Gender identity',
+  //         value_emojified: 'Soyboy',
+  //         value_plain: 'Soyboy'
+  //       }
+  //     ],
+  //     pleroma: {
+  //       hide_follows: false,
+  //       hide_followers_count: false,
+  //       background_image: null,
+  //       confirmation_pending: false,
+  //       is_moderator: false,
+  //       hide_follows_count: false,
+  //       hide_followers: false,
+  //       relationship: {
+  //         showing_reblogs: true,
+  //         followed_by: false,
+  //         subscribing: false,
+  //         blocked_by: false,
+  //         requested: false,
+  //         domain_blocking: false,
+  //         following: false,
+  //         endorsed: false,
+  //         blocking: false,
+  //         muting: false,
+  //         id: '9w1HhmenIAKBHJiUs4',
+  //         muting_notifications: false
+  //       },
+  //       tags: [],
+  //       hide_favorites: true,
+  //       is_admin: false,
+  //       skip_thread_containment: false
+  //     },
+  //     source: {
+  //       fields: [],
+  //       note: 'Fediverse developer. I come in peace. #vegan #freeculture #atheist #antiporn #gendercritical. Boosts ≠ endorsements.',
+  //       pleroma: {
+  //         actor_type: 'Person',
+  //         discoverable: false
+  //       },
+  //       sensitive: false
+  //     },
+  //     id: '9w1HhmenIAKBHJiUs4',
+  //     note_emojified: 'Fediverse developer. I come in peace. <a class="hashtag" data-tag="vegan" href="https://gleasonator.com/tag/vegan">#vegan</a> <a class="hashtag" data-tag="freeculture" href="https://gleasonator.com/tag/freeculture">#freeculture</a> <a class="hashtag" data-tag="atheist" href="https://gleasonator.com/tag/atheist">#atheist</a> <a class="hashtag" data-tag="antiporn" href="https://gleasonator.com/tag/antiporn">#antiporn</a> <a class="hashtag" data-tag="gendercritical" href="https://gleasonator.com/tag/gendercritical">#gendercritical</a>. Boosts ≠ endorsements.'
+  //   }
+  // };
+  //     const action = {
+  //       type: actions.COMPOSE_MENTION,
+  //       account: account,
+  //     };
+  //     expect(reducer(state, action).toJS()).toMatchObject({
+  //       text: '@alex@gleasonator.com',
+  //       caretPosition: null,
+  //     });
+  //   });
 
   // it('should handle COMPOSE_DIRECT', () => {
   //   const state = ImmutableMap({ caretPosition: 0, privacy: 'public' });
