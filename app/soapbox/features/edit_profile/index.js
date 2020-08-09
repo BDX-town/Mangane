9a2fba1ef5a5340ce122ee951e9015d27ac52d1d
Alex Gleason <alex@alexgleason.me>
2020-08-08 20:32:39 -0500

    Revert "Revert "Store fieldsLimits in instance/pleroma/metadata of the Redux store.  Render fields to maxFields value in features/edit_profile, using maxFields value""
    This reverts commit b3a6c395dffb9147ebe7b9d530bf8d22e1b2914d.

--- app/soapbox/features/edit_profile/index.js
+++ app/soapbox/features/edit_profile/index.js
@@ -20,8 +20,6 @@ import {
 import { patchMe } from 'soapbox/actions/me';
 import { unescape } from 'lodash';
 
-const MAX_FIELDS = 4; // TODO: Make this dynamic by the instance
-
 const messages = defineMessages({
   heading: { id: 'column.edit_profile', defaultMessage: 'Edit profile' },
   metaFieldLabel: { id: 'edit_profile.fields.meta_fields.label_placeholder', defaultMessage: 'Label' },
@@ -33,12 +31,13 @@ const mapStateToProps = state => {
   const me = state.get('me');
   return {
     account: state.getIn(['accounts', me]),
+    maxFields: state.getIn(['instance', 'pleroma', 'metadata', 'fieldsLimits', 'maxFields']),
   };
 };
 
-// Forces fields to be MAX_SIZE, filling empty values
-const normalizeFields = fields => (
-  ImmutableList(fields).setSize(MAX_FIELDS).map(field =>
+// Forces fields to be maxFields size, filling empty values
+const normalizeFields = (fields, maxFields) => (
+  ImmutableList(fields).setSize(maxFields).map(field =>
     field ? field : ImmutableMap({ name: '', value: '' })
   )
 );
@@ -58,11 +57,11 @@ class EditProfile extends ImmutablePureComponent {
     dispatch: PropTypes.func.isRequired,
     intl: PropTypes.object.isRequired,
     account: ImmutablePropTypes.map,
+    maxFields: PropTypes.number,
   };
 
   state = {
     isLoading: false,
-    fields: normalizeFields(Array.from({ length: MAX_FIELDS })),
   }
 
   constructor(props) {
@@ -70,7 +69,7 @@ class EditProfile extends ImmutablePureComponent {
     const initialState = props.account.withMutations(map => {
       map.merge(map.get('source'));
       map.delete('source');
-      map.set('fields', normalizeFields(map.get('fields')));
+      map.set('fields', normalizeFields(map.get('fields'), props.maxFields));
       unescapeParams(map, ['display_name', 'note']);
     });
     this.state = initialState.toObject();
@@ -157,7 +156,7 @@ class EditProfile extends ImmutablePureComponent {
   }
 
   render() {
-    const { intl, account } = this.props;
+    const { intl, maxFields, account } = this.props;
     const verified = account.get('pleroma').get('tags').includes('verified');
 
     return (
@@ -219,7 +218,7 @@ class EditProfile extends ImmutablePureComponent {
                 <div className='input with_block_label'>
                   <label><FormattedMessage id='edit_profile.fields.meta_fields_label' defaultMessage='Profile metadata' /></label>
                   <span className='hint'>
-                    <FormattedMessage id='edit_profile.hints.meta_fields' defaultMessage='You can have up to {count, plural, one {# item} other {# items}} displayed as a table on your profile' values={{ count: MAX_FIELDS }} />
+                    <FormattedMessage id='edit_profile.hints.meta_fields' defaultMessage='You can have up to {count, plural, one {# item} other {# items}} displayed as a table on your profile' values={{ count: maxFields }} />
                   </span>
                   {
                     this.state.fields.map((field, i) => (
