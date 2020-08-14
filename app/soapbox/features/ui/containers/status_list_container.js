fdbe981477039013aaf22cd8288447f4198e9d39
Alex Gleason <alex@alexgleason.me>
2020-08-14 15:28:09 -0500

    Fix timeline queue header when filters are enabled

--- app/soapbox/features/ui/containers/status_list_container.js
+++ app/soapbox/features/ui/containers/status_list_container.js
@@ -6,6 +6,7 @@ import { debounce } from 'lodash';
 import { dequeueTimeline } from 'soapbox/actions/timelines';
 import { scrollTopTimeline } from '../../../actions/timelines';
 import { getSettings } from 'soapbox/actions/settings';
+import { shouldFilter } from 'soapbox/utils/timelines';
 
 const makeGetStatusIds = () => createSelector([
   (state, { type }) => getSettings(state).get(type, ImmutableMap()),
@@ -14,24 +15,9 @@ const makeGetStatusIds = () => createSelector([
   (state)           => state.get('me'),
 ], (columnSettings, statusIds, statuses, me) => {
   return statusIds.filter(id => {
-    if (id === null) return true;
-
-    const statusForId = statuses.get(id);
-    let showStatus    = true;
-
-    if (columnSettings.getIn(['shows', 'reblog']) === false) {
-      showStatus = showStatus && statusForId.get('reblog') === null;
-    }
-
-    if (columnSettings.getIn(['shows', 'reply']) === false) {
-      showStatus = showStatus && (statusForId.get('in_reply_to_id') === null);
-    }
-
-    if (columnSettings.getIn(['shows', 'direct']) === false) {
-      showStatus = showStatus && (statusForId.get('visibility') !== 'direct');
-    }
-
-    return showStatus;
+    const status = statuses.get(id);
+    if (!status) return true;
+    return !shouldFilter(status, columnSettings);
   });
 });
 
