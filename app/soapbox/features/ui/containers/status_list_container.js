6fb6356b9990ef718e87b986415f488ca556fd38
Mary Kate <mk@marykatefain.com>
2020-08-13 21:04:12 -0500

    Revert "initial pass at fixing counter, doesn't work"
    This reverts commit 13e3095cd102da3f4eaabe1a940f44e78f4e66db.

--- app/soapbox/features/ui/containers/status_list_container.js
+++ app/soapbox/features/ui/containers/status_list_container.js
@@ -38,21 +38,12 @@ const makeGetStatusIds = () => createSelector([
 const mapStateToProps = (state, { timelineId }) => {
   const getStatusIds = makeGetStatusIds();
 
-  const queuedItems = state.getIn(['timelines', timelineId, 'queuedItems'], ImmutableList());
-  console.log('queuedItems:', queuedItems);
-
-  const filteredQueuedItems = queuedItems.filter((id) => getStatusIds(state, { type: timelineId }).includes(id));
-  console.log('filteredQueuedItems:', filteredQueuedItems);
-
-  const filteredQueuedItemsCount = filteredQueuedItems.count();
-  console.log('filteredQueuedItemsCount:', filteredQueuedItemsCount);
-
   return {
     statusIds: getStatusIds(state, { type: timelineId }),
     isLoading: state.getIn(['timelines', timelineId, 'isLoading'], true),
     isPartial: state.getIn(['timelines', timelineId, 'isPartial'], false),
     hasMore:   state.getIn(['timelines', timelineId, 'hasMore']),
-    totalQueuedItemsCount: filteredQueuedItemsCount,
+    totalQueuedItemsCount: state.getIn(['timelines', timelineId, 'totalQueuedItemsCount']),
   };
 };
 
