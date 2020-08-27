44f7ad5e1cfc8c9677352265ac651fb89f05b471
Alex Gleason <alex@alexgleason.me>
2020-08-27 13:45:37 -0500

    Chats: sort chats in reducer instead of component

--- app/soapbox/reducers/chats.js
+++ app/soapbox/reducers/chats.js
@@ -8,16 +8,27 @@ const importChat = (state, chat) => state.set(chat.id, fromJS(normalizeChat(chat
 const importChats = (state, chats) =>
   state.withMutations(mutable => chats.forEach(chat => importChat(mutable, chat)));
 
+const chatDateComparator = (chatA, chatB) => {
+  // Sort most recently updated chats at the top
+  const a = new Date(chatA.get('updated_at'));
+  const b = new Date(chatB.get('updated_at'));
+
+  if (a === b) return 0;
+  if (a > b) return -1;
+  if (a < b) return 1;
+  return 0;
+};
+
 const initialState = ImmutableMap();
 
 export default function chats(state = initialState, action) {
   switch(action.type) {
   case CHATS_FETCH_SUCCESS:
-    return importChats(state, action.chats);
+    return importChats(state, action.chats).sort(chatDateComparator);
   case STREAMING_CHAT_UPDATE:
-    return importChats(state, [action.chat]);
+    return importChats(state, [action.chat]).sort(chatDateComparator);
   case CHAT_FETCH_SUCCESS:
-    return importChats(state, [action.chat]);
+    return importChats(state, [action.chat]).sort(chatDateComparator);
   default:
     return state;
   }
