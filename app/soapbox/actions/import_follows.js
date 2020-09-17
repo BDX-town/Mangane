1b8afd22486fbdd07c031009556ed700ebb77c3a
crockwave <curtis.rock@wirelessmessaging.com>
2020-09-17 17:33:18 -0500

    Convert newline delimited data to space delimited data

--- app/soapbox/actions/import_follows.js
+++ app/soapbox/actions/import_follows.js
@@ -4,11 +4,16 @@ export const IMPORT_FOLLOWS_REQUEST = 'IMPORT_FOLLOWS_REQUEST';
 export const IMPORT_FOLLOWS_SUCCESS = 'IMPORT_FOLLOWS_SUCCESS';
 export const IMPORT_FOLLOWS_FAIL    = 'IMPORT_FOLLOWS_FAIL';
 
+function whiteSpace(params) {
+  const follows = params.replace(/\n/g, ' ');
+  return follows;
+};
+
 export function importFollows(params) {
   return (dispatch, getState) => {
     dispatch({ type: IMPORT_FOLLOWS_REQUEST });
     return api(getState)
-      .post('/api/pleroma/follow_import', params)
+      .post('/api/pleroma/follow_import', whiteSpace(params))
       .then(response => {
         dispatch({ type: IMPORT_FOLLOWS_SUCCESS, config: response.data });
       }).catch(error => {
