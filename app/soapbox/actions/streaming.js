f643863eccc77cda886b94c20e27b5eaa09f3216
Alex Gleason <alex@alexgleason.me>
2020-09-17 23:27:57 -0500

    Chat notifs: only play sound for recipient message

--- app/soapbox/actions/streaming.js
+++ app/soapbox/actions/streaming.js
@@ -55,11 +55,16 @@ export function connectTimelineStream(timelineId, path, pollingRefresh = null, a
           dispatch(fetchFilters());
           break;
         case 'pleroma:chat_update':
-          dispatch({
-            type: STREAMING_CHAT_UPDATE,
-            chat: JSON.parse(data.payload),
-            me: getState().get('me'),
-            meta: getSettings(getState()).getIn(['chats', 'sound']) && { sound: 'chat' },
+          dispatch((dispatch, getState) => {
+            const chat = JSON.parse(data.payload);
+            const messageOwned = !(chat.last_message && chat.last_message.account_id !== getState().get('me'));
+
+            dispatch({
+              type: STREAMING_CHAT_UPDATE,
+              chat,
+              // Only play sounds for recipient messages
+              meta: !messageOwned && getSettings(getState()).getIn(['chats', 'sound']) && { sound: 'chat' },
+            });
           });
           break;
         }
