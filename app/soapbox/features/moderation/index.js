649b02072ba1e6ad43e6056419f8150a54cb5ac5
Mary Kate <mk@marykatefain.com>
2020-10-14 13:40:26 -0500

    basic loading of open report data into moderation index component

--- app/soapbox/features/moderation/index.js
+++ app/soapbox/features/moderation/index.js
@@ -6,6 +6,8 @@ import PropTypes from 'prop-types';
 import ImmutablePropTypes from 'react-immutable-proptypes';
 import snackbar from 'soapbox/actions/snackbar';
 import Column from '../ui/components/column';
+import ScrollableList from '../../components/scrollable_list';
+import LoadGap from '../../components/load_gap';
 import {
   SimpleForm,
   FieldsGroup,
@@ -14,13 +16,16 @@ import {
   FileChooser,
   SimpleTextarea,
 } from 'soapbox/features/forms';
-
+import {
+  fetchReports,
+} from '../../actions/admin';
 import {
   Map as ImmutableMap,
   List as ImmutableList,
 } from 'immutable';
 import { patchMe } from 'soapbox/actions/me';
-import { unescape } from 'lodash';
+import { unescape, debounce } from 'lodash';
+
 
 const messages = defineMessages({
   heading: { id: 'column.moderation', defaultMessage: 'Moderation' },
@@ -29,7 +34,8 @@ const messages = defineMessages({
 
 const mapStateToProps = state => {
   return {
-
+    open_report_count: state.getIn(['admin', 'open_report_count']),
+    open_reports: state.getIn(['admin', 'reports']),
   };
 };
 
@@ -46,14 +52,65 @@ class Moderation extends ImmutablePureComponent {
     isLoading: false,
   }
 
+  componentWillUnmount() {
+    this.handleLoadOlder.cancel();
+  }
+
+  componentDidMount() {
+    this.props.dispatch(fetchReports({ state: 'open' }));
+  }
+
+  handleLoadGap = () => {
+    this.props.dispatch(fetchReports({ state: 'open' }));
+  };
+
+  handleLoadOlder = debounce(() => {
+    const last = this.props.open_reports.last();
+    this.props.dispatch(fetchReports({ state: 'open' }));
+  }, 300, { leading: true });
 
 
   render() {
-    const { intl } = this.props;
+    const { intl, open_reports, isLoading, hasMore, open_report_count } = this.props;
+    const emptyMessage = <FormattedMessage id='empty_column.open_reports' defaultMessage="There are no open reports." />;
+
+    let scrollableContent = null;
+
+    if (isLoading && this.scrollableContent) {
+      scrollableContent = this.scrollableContent;
+    } else if (open_reports.size > 0 || hasMore) {
+      scrollableContent = open_reports.map((item, index) => item === null ? (
+        <LoadGap
+          key={'gap:' + open_reports.getIn([index + 1, 'id'])}
+          disabled={isLoading}
+          maxId={index > 0 ? open_reports.getIn([index - 1, 'id']) : null}
+          onClick={this.handleLoadGap}
+        />
+      ) : (
+        <FormattedMessage id='empty_column.report_test' defaultMessage="Open Report Item" />
+        // <ReportContainer
+        //   key={item.get('id')}
+        //   report={item}
+        //   accountId={item.get('account')}
+        // />
+      ));
+    } else {
+      scrollableContent = null;
+    }
+
+    this.scrollableContent = scrollableContent;
 
     return (
       <Column icon='gavel' heading={intl.formatMessage(messages.heading)} backBtnSlim>
-
+      <ScrollableList
+        scrollKey='open_reports'
+        isLoading={isLoading}
+        showLoading={isLoading && open_reports.size === 0}
+        hasMore={hasMore}
+        emptyMessage={emptyMessage}
+      >
+        {scrollableContent}
+      </ScrollableList>
       </Column>
     );
   }
