d769ee7c901dfc11847f9fe239cadc6aad294464
Sean King <seanking2919@protonmail.com>
2020-11-10 17:22:02 -0700

    Refactor promo and features panels

--- app/soapbox/features/ui/components/features_panel.js
+++ app/soapbox/features/ui/components/features_panel.js
@@ -27,40 +27,31 @@ class FeaturesPanel extends React.PureComponent {
       <div className='wtf-panel promo-panel'>
         <div className='promo-panel__container'>
 
-          <div className='promo-panel-item'>
-            <NavLink className='promo-panel-item__btn' to='/settings/profile'>
-              <Icon id='user' className='promo-panel-item__icon' fixedWidth />
-              {intl.formatMessage(messages.edit_profile)}
-            </NavLink>
-          </div>
+          <NavLink className='promo-panel-item' to='/settings/profile'>
+            <Icon id='user' className='promo-panel-item__icon' fixedWidth />
+            {intl.formatMessage(messages.edit_profile)}
+          </NavLink>
 
-          <div className='promo-panel-item'>
-            <NavLink className='promo-panel-item__btn' to='/bookmarks'>
-              <Icon id='bookmark' className='promo-panel-item__icon' fixedWidth />
-              {intl.formatMessage(messages.bookmarks)}
-            </NavLink>
-          </div>
 
-          <div className='promo-panel-item'>
-            <NavLink className='promo-panel-item__btn' to='/lists'>
-              <Icon id='list' className='promo-panel-item__icon' fixedWidth />
-              {intl.formatMessage(messages.lists)}
-            </NavLink>
-          </div>
+          <NavLink className='promo-panel-item' to='/bookmarks'>
+            <Icon id='bookmark' className='promo-panel-item__icon' fixedWidth />
+            {intl.formatMessage(messages.bookmarks)}
+          </NavLink>
 
-          <div className='promo-panel-item'>
-            <NavLink className='promo-panel-item__btn' to='/auth/edit'>
-              <Icon id='lock' className='promo-panel-item__icon' fixedWidth />
-              {intl.formatMessage(messages.security)}
-            </NavLink>
-          </div>
+          <NavLink className='promo-panel-item' to='/lists'>
+            <Icon id='list' className='promo-panel-item__icon' fixedWidth />
+            {intl.formatMessage(messages.lists)}
+          </NavLink>
 
-          <div className='promo-panel-item'>
-            <NavLink className='promo-panel-item__btn' to='/settings/preferences'>
-              <Icon id='cog' className='promo-panel-item__icon' fixedWidth />
-              {intl.formatMessage(messages.preferences)}
-            </NavLink>
-          </div>
+          <NavLink className='promo-panel-item' to='/auth/edit'>
+            <Icon id='lock' className='promo-panel-item__icon' fixedWidth />
+            {intl.formatMessage(messages.security)}
+          </NavLink>
+
+          <NavLink className='promo-panel-item' to='/settings/preferences'>
+            <Icon id='cog' className='promo-panel-item__icon' fixedWidth />
+            {intl.formatMessage(messages.preferences)}
+          </NavLink>
 
         </div>
       </div>
