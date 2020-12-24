3d4bd238707a12c466c64e699e369774edc1118a
Alex Gleason <alex@alexgleason.me>
2020-12-24 14:40:47 -0600

    Fix emoji heart tests

--- app/soapbox/utils/emoji_reacts.js
+++ app/soapbox/utils/emoji_reacts.js
@@ -75,11 +75,12 @@ export const reduceEmoji = (emojiReacts, favouritesCount, favourited, allowedEmo
     emojiReacts, favouritesCount, favourited,
   ))), allowedEmoji));
 
-export const getReactForStatus = status => {
+export const getReactForStatus = (status, allowedEmoji=ALLOWED_EMOJI) => {
   return reduceEmoji(
     status.getIn(['pleroma', 'emoji_reactions'], ImmutableList()),
-    status.get('favourites_count'),
+    status.get('favourites_count', 0),
     status.get('favourited'),
+    allowedEmoji,
   ).filter(e => e.get('me') === true)
     .getIn([0, 'name']);
 };
