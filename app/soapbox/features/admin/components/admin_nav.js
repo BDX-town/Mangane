0ccc113931eae73de6cf96bbdd4088440b31531c
Alex Gleason <alex@alexgleason.me>
2020-12-29 14:16:19 -0600

    Admon: add dummy placeholder links

--- app/soapbox/features/admin/components/admin_nav.js
+++ app/soapbox/features/admin/components/admin_nav.js
@@ -20,25 +20,70 @@ class AdminNav extends React.PureComponent {
     const { instance } = this.props;
 
     return (
-      <div className='wtf-panel promo-panel'>
-        <div className='promo-panel__container'>
-          <NavLink className='promo-panel-item' to='/admin'>
-            <Icon id='tachometer' className='promo-panel-item__icon' fixedWidth />
-            <FormattedMessage id='admin_nav.dashboard' defaultMessage='Dashboard' />
-          </NavLink>
-          {/* TODO: Make this actually useful */}
-          {instance.get('approval_required') && (
-            <a className='promo-panel-item' href='/pleroma/admin/#/users/index' target='_blank'>
-              <Icon id='user' className='promo-panel-item__icon' fixedWidth />
-              <FormattedMessage id='admin_nav.awaiting_approval' defaultMessage='Awaiting Approval' />
+      <>
+        <div className='wtf-panel promo-panel'>
+          <div className='promo-panel__container'>
+            <NavLink className='promo-panel-item' to='/admin'>
+              <Icon id='tachometer' className='promo-panel-item__icon' fixedWidth />
+              <FormattedMessage id='admin_nav.dashboard' defaultMessage='Dashboard' />
+            </NavLink>
+            <a className='promo-panel-item' href='/pleroma/admin/#/reports/index' target='_blank'>
+              <Icon id='gavel' className='promo-panel-item__icon' fixedWidth />
+              <FormattedMessage id='admin_nav.reports' defaultMessage='Reports' />
             </a>
-          )}
-          <a className='promo-panel-item' href='/pleroma/admin/#/reports/index' target='_blank'>
-            <Icon id='gavel' className='promo-panel-item__icon' fixedWidth />
-            <FormattedMessage id='admin_nav.reports' defaultMessage='Reports' />
-          </a>
+            {instance.get('approval_required') && (
+              <a className='promo-panel-item' href='/pleroma/admin/#/users/index' target='_blank'>
+                <Icon id='user' className='promo-panel-item__icon' fixedWidth />
+                <FormattedMessage id='admin_nav.awaiting_approval' defaultMessage='Awaiting Approval' />
+              </a>
+            )}
+            <NavLink className='promo-panel-item' to='#'>
+              <Icon id='group' className='promo-panel-item__icon' fixedWidth />
+              <FormattedMessage id='admin_nav.registration' defaultMessage='Registration' />
+            </NavLink>
+            <NavLink className='promo-panel-item' to='#'>
+              <Icon id='envelope' className='promo-panel-item__icon' fixedWidth />
+              <FormattedMessage id='admin_nav.invites' defaultMessage='Invites' />
+            </NavLink>
+          </div>
         </div>
-      </div>
+        <div className='wtf-panel promo-panel'>
+          <div className='promo-panel__container'>
+            <NavLink className='promo-panel-item' to='#'>
+              <Icon id='info-circle' className='promo-panel-item__icon' fixedWidth />
+              <FormattedMessage id='admin_nav.site_profile' defaultMessage='Site Profile' />
+            </NavLink>
+            <NavLink className='promo-panel-item' to='#'>
+              <Icon id='paint-brush' className='promo-panel-item__icon' fixedWidth />
+              <FormattedMessage id='admin_nav.branding' defaultMessage='Branding' />
+            </NavLink>
+            <NavLink className='promo-panel-item' to='#'>
+              <Icon id='bars' className='promo-panel-item__icon' fixedWidth />
+              <FormattedMessage id='admin_nav.menus' defaultMessage='Menus' />
+            </NavLink>
+            <NavLink className='promo-panel-item' to='#'>
+              <Icon id='file-o' className='promo-panel-item__icon' fixedWidth />
+              <FormattedMessage id='admin_nav.pages' defaultMessage='Pages' />
+            </NavLink>
+          </div>
+        </div>
+        <div className='wtf-panel promo-panel'>
+          <div className='promo-panel__container'>
+            <NavLink className='promo-panel-item' to='#'>
+              <Icon id='fediverse' className='promo-panel-item__icon' fixedWidth />
+              <FormattedMessage id='admin_nav.mrf' defaultMessage='Federation' />
+            </NavLink>
+            <NavLink className='promo-panel-item' to='#'>
+              <Icon id='filter' className='promo-panel-item__icon' fixedWidth />
+              <FormattedMessage id='admin_nav.filtering' defaultMessage='Filtering' />
+            </NavLink>
+            <a className='promo-panel-item' href='/pleroma/admin/#/settings/index' target='_blank'>
+              <Icon id='code' className='promo-panel-item__icon' fixedWidth />
+              <FormattedMessage id='admin_nav.advanced' defaultMessage='Advanced' />
+            </a>
+          </div>
+        </div>
+      </>
     );
   }
 
