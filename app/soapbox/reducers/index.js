ddac13d308f867d07dc319c108641438efc23f77
Alex Gleason <alex@alexgleason.me>
2021-01-07 14:17:06 -0600

    Account backups

--- app/soapbox/reducers/index.js
+++ app/soapbox/reducers/index.js
@@ -49,6 +49,7 @@ import chats from './chats';
 import chat_messages from './chat_messages';
 import chat_message_lists from './chat_message_lists';
 import profile_hover_card from './profile_hover_card';
+import backups from './backups';
 
 const appReducer = combineReducers({
   dropdown_menu,
@@ -99,6 +100,7 @@ const appReducer = combineReducers({
   chat_messages,
   chat_message_lists,
   profile_hover_card,
+  backups,
 });
 
 // Clear the state (mostly) when the user logs out
