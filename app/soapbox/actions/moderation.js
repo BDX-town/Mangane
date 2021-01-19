498471958b9962e9fa8addeef44aaa8ed6baaf9d
Alex Gleason <alex@alexgleason.me>
2021-01-18 18:25:36 -0600

    Use short names for admin quick actions

--- app/soapbox/actions/moderation.js
+++ app/soapbox/actions/moderation.js
@@ -4,27 +4,29 @@ import { deactivateUsers, deleteUsers, deleteStatus } from 'soapbox/actions/admi
 import snackbar from 'soapbox/actions/snackbar';
 
 const messages = defineMessages({
-  deactivateUserPrompt: { id: 'confirmations.admin.deactivate_user.message', defaultMessage: 'You are about to deactivate {acct}. Deactivating a user is a reversible action.' },
-  deactivateUserConfirm: { id: 'confirmations.admin.deactivate_user.confirm', defaultMessage: 'Deactivate {acct}' },
-  userDeactivated: { id: 'admin.users.user_deactivated_message', defaultMessage: '{acct} was deactivated' },
-  deleteUserPrompt: { id: 'confirmations.admin.delete_user.message', defaultMessage: 'You are about to delete {acct}. THIS IS A DESTRUCTIVE ACTION THAT CANNOT BE UNDONE.' },
-  deleteUserConfirm: { id: 'confirmations.admin.delete_user.confirm', defaultMessage: 'Delete {acct}' },
-  userDeleted: { id: 'admin.users.user_deleted_message', defaultMessage: '{acct} was deleted' },
-  deleteStatusPrompt: { id: 'confirmations.admin.delete_status.message', defaultMessage: 'You are about to delete a post by {acct}. This action cannot be undone.' },
+  deactivateUserPrompt: { id: 'confirmations.admin.deactivate_user.message', defaultMessage: 'You are about to deactivate @{acct}. Deactivating a user is a reversible action.' },
+  deactivateUserConfirm: { id: 'confirmations.admin.deactivate_user.confirm', defaultMessage: 'Deactivate @{name}' },
+  userDeactivated: { id: 'admin.users.user_deactivated_message', defaultMessage: '@{acct} was deactivated' },
+  deleteUserPrompt: { id: 'confirmations.admin.delete_user.message', defaultMessage: 'You are about to delete @{acct}. THIS IS A DESTRUCTIVE ACTION THAT CANNOT BE UNDONE.' },
+  deleteUserConfirm: { id: 'confirmations.admin.delete_user.confirm', defaultMessage: 'Delete @{name}' },
+  userDeleted: { id: 'admin.users.user_deleted_message', defaultMessage: '@{acct} was deleted' },
+  deleteStatusPrompt: { id: 'confirmations.admin.delete_status.message', defaultMessage: 'You are about to delete a post by @{acct}. This action cannot be undone.' },
   deleteStatusConfirm: { id: 'confirmations.admin.delete_status.confirm', defaultMessage: 'Delete post' },
-  statusDeleted: { id: 'admin.statuses.status_deleted_message', defaultMessage: 'Post by {acct} was deleted' },
+  statusDeleted: { id: 'admin.statuses.status_deleted_message', defaultMessage: 'Post by @{acct} was deleted' },
 });
 
 export function deactivateUserModal(intl, accountId, afterConfirm = () => {}) {
   return function(dispatch, getState) {
     const state = getState();
     const acct = state.getIn(['accounts', accountId, 'acct']);
+    const name = state.getIn(['accounts', accountId, 'username']);
+
     dispatch(openModal('CONFIRM', {
-      message: intl.formatMessage(messages.deactivateUserPrompt, { acct: `@${acct}` }),
-      confirm: intl.formatMessage(messages.deactivateUserConfirm, { acct: `@${acct}` }),
+      message: intl.formatMessage(messages.deactivateUserPrompt, { acct }),
+      confirm: intl.formatMessage(messages.deactivateUserConfirm, { name }),
       onConfirm: () => {
         dispatch(deactivateUsers([acct])).then(() => {
-          const message = intl.formatMessage(messages.userDeactivated, { acct: `@${acct}` });
+          const message = intl.formatMessage(messages.userDeactivated, { acct });
           dispatch(snackbar.success(message));
           afterConfirm();
         }).catch(() => {});
@@ -37,12 +39,14 @@ export function deleteUserModal(intl, accountId, afterConfirm = () => {}) {
   return function(dispatch, getState) {
     const state = getState();
     const acct = state.getIn(['accounts', accountId, 'acct']);
+    const name = state.getIn(['accounts', accountId, 'username']);
+
     dispatch(openModal('CONFIRM', {
-      message: intl.formatMessage(messages.deleteUserPrompt, { acct: `@${acct}` }),
-      confirm: intl.formatMessage(messages.deleteUserConfirm, { acct: `@${acct}` }),
+      message: intl.formatMessage(messages.deleteUserPrompt, { acct }),
+      confirm: intl.formatMessage(messages.deleteUserConfirm, { name }),
       onConfirm: () => {
         dispatch(deleteUsers([acct])).then(() => {
-          const message = intl.formatMessage(messages.userDeleted, { acct: `@${acct}` });
+          const message = intl.formatMessage(messages.userDeleted, { acct });
           dispatch(snackbar.success(message));
           afterConfirm();
         }).catch(() => {});
@@ -58,11 +62,11 @@ export function deleteStatusModal(intl, statusId, afterConfirm = () => {}) {
     const acct = state.getIn(['accounts', accountId, 'acct']);
 
     dispatch(openModal('CONFIRM', {
-      message: intl.formatMessage(messages.deleteStatusPrompt, { acct: `@${acct}` }),
+      message: intl.formatMessage(messages.deleteStatusPrompt, { acct }),
       confirm: intl.formatMessage(messages.deleteStatusConfirm),
       onConfirm: () => {
         dispatch(deleteStatus(statusId)).then(() => {
-          const message = intl.formatMessage(messages.statusDeleted, { acct: `@${acct}` });
+          const message = intl.formatMessage(messages.statusDeleted, { acct });
           dispatch(snackbar.success(message));
         }).catch(() => {});
         afterConfirm();
