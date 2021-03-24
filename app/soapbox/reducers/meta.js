6b8fbbff47607d10035b73402c8f7a2a8d9a28ef
Alex Gleason <alex@alexgleason.me>
2021-03-23 21:04:12 -0500

    Remove dead code

--- app/soapbox/reducers/meta.js
+++ app/soapbox/reducers/meta.js
@@ -1,8 +1,7 @@
 'use strict';
 
 import { ME_FETCH_SUCCESS, ME_PATCH_SUCCESS } from 'soapbox/actions/me';
-import { VERIFY_CREDENTIALS_SUCCESS } from 'soapbox/actions/auth';
-import { Map as ImmutableMap, OrderedSet as ImmutableOrderedSet, fromJS } from 'immutable';
+import { Map as ImmutableMap, fromJS } from 'immutable';
 
 const initialState = ImmutableMap();
 
@@ -12,15 +11,11 @@ export default function meta(state = initialState, action) {
   case ME_PATCH_SUCCESS:
     const me = fromJS(action.me);
     return state.withMutations(state => {
-      state.set('me', me.get('id'));
-      state.update('users', ImmutableOrderedSet(), v => v.add(me.get('id')));
       if (me.has('pleroma')) {
         const pleroPrefs = me.get('pleroma').delete('settings_store');
         state.mergeIn(['pleroma'], pleroPrefs);
       }
     });
-  case VERIFY_CREDENTIALS_SUCCESS:
-    return state.update('users', ImmutableOrderedSet(), v => v.add(action.account.id));
   default:
     return state;
   }
