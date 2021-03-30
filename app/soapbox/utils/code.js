a032369d33ac518cba529f6fd3bc15d748f170a7
Alex Gleason <alex@alexgleason.me>
2021-03-30 13:11:04 -0500

    Include Soapbox FE commit hash when git is available

--- app/soapbox/utils/code.js
+++ app/soapbox/utils/code.js
@@ -1,11 +1,27 @@
 // @preval
 const pkg = require('../../../package.json');
+const { execSync } = require('child_process');
 
 const shortRepoName = url => new URL(url).pathname.substring(1);
+
+const version = pkg => {
+  try {
+    const head = String(execSync('git rev-parse HEAD'));
+    const tag = String(execSync(`git rev-parse v${pkg.version}`));
+
+    if (head !== tag) {
+      return `${pkg.version}-${head.substring(0, 7)}`;
+    } else {
+      return pkg.version;
+    }
+  } catch (e) {
+    return pkg.version;
+  }
+};
 
 module.exports = {
   name: pkg.name,
   url: pkg.repository.url,
   repository: shortRepoName(pkg.repository.url),
-  version: pkg.version,
+  version: version(pkg),
 };
