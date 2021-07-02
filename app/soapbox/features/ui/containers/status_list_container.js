352c87c3b7534f473139a5668b2de7ca17e077b2
Alex Gleason <alex@alexgleason.me>
2021-07-01 19:41:16 -0500

    Fix StatusList memoization

--- app/soapbox/features/ui/containers/status_list_container.js
+++ app/soapbox/features/ui/containers/status_list_container.js
@@ -21,18 +21,23 @@ const makeGetStatusIds = () => createSelector([
   });
 });
 
-const mapStateToProps = (state, { timelineId }) => {
-  const lastStatusId = state.getIn(['timelines', timelineId, 'items'], ImmutableList()).last();
+const makeMapStateToProps = () => {
   const getStatusIds = makeGetStatusIds();
 
-  return {
-    statusIds: getStatusIds(state, { type: timelineId }),
-    lastStatusId: lastStatusId,
-    isLoading: state.getIn(['timelines', timelineId, 'isLoading'], true),
-    isPartial: state.getIn(['timelines', timelineId, 'isPartial'], false),
-    hasMore:   state.getIn(['timelines', timelineId, 'hasMore']),
-    totalQueuedItemsCount: state.getIn(['timelines', timelineId, 'totalQueuedItemsCount']),
+  const mapStateToProps = (state, { timelineId }) => {
+    const lastStatusId = state.getIn(['timelines', timelineId, 'items'], ImmutableList()).last();
+
+    return {
+      statusIds: getStatusIds(state, { type: timelineId }),
+      lastStatusId: lastStatusId,
+      isLoading: state.getIn(['timelines', timelineId, 'isLoading'], true),
+      isPartial: state.getIn(['timelines', timelineId, 'isPartial'], false),
+      hasMore:   state.getIn(['timelines', timelineId, 'hasMore']),
+      totalQueuedItemsCount: state.getIn(['timelines', timelineId, 'totalQueuedItemsCount']),
+    };
   };
+
+  return mapStateToProps;
 };
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
@@ -47,4 +52,4 @@ const mapDispatchToProps = (dispatch, ownProps) => ({
   }, 100),
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(StatusList);
+export default connect(makeMapStateToProps, mapDispatchToProps)(StatusList);
