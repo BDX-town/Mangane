60bdccc0b5ea00ffe8a2c0250c43a8dce5319667
Alex Gleason <alex@alexgleason.me>
2021-07-09 17:02:04 -0500

    Big emojis: fix nodeList count

--- app/soapbox/utils/rich_content.js
+++ app/soapbox/utils/rich_content.js
@@ -2,10 +2,10 @@
 export const onlyEmoji = (node, limit = 1) => {
   if (!node) return false;
   if (node.textContent.replaceAll(' ', '') !== '') return false;
-  const emojis = [...node.querySelectorAll('img.emojione')];
+  const emojis = Array.from(node.querySelectorAll('img.emojione'));
   if (emojis.length === 0) return false;
   if (emojis.length > limit) return false;
-  const images = [...node.querySelectorAll('img')];
+  const images = Array.from(node.querySelectorAll('img'));
   if (images.length > emojis.length) return false;
   return true;
 };
