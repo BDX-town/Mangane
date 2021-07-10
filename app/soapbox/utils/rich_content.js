a66f2b3f757e294a1ad9abd830c635a594875b17
Alex Gleason <alex@alexgleason.me>
2021-07-10 12:41:57 -0500

    Big emojis: fix possible web crash

--- app/soapbox/utils/rich_content.js
+++ app/soapbox/utils/rich_content.js
@@ -2,17 +2,24 @@
 export const onlyEmoji = (node, limit = 1, ignoreMentions = true) => {
   if (!node) return false;
 
-  // Remove mentions before checking content
-  if (ignoreMentions) {
-    node = node.cloneNode(true);
-    node.querySelectorAll('a.mention').forEach(m => m.parentNode.removeChild(m));
-  }
+  try {
+    // Remove mentions before checking content
+    if (ignoreMentions) {
+      node = node.cloneNode(true);
+      node.querySelectorAll('a.mention').forEach(m => m.parentNode.removeChild(m));
+    }
 
-  if (node.textContent.replaceAll(' ', '') !== '') return false;
-  const emojis = Array.from(node.querySelectorAll('img.emojione'));
-  if (emojis.length === 0) return false;
-  if (emojis.length > limit) return false;
-  const images = Array.from(node.querySelectorAll('img'));
-  if (images.length > emojis.length) return false;
-  return true;
+    if (node.textContent.replaceAll(' ', '') !== '') return false;
+    const emojis = Array.from(node.querySelectorAll('img.emojione'));
+    if (emojis.length === 0) return false;
+    if (emojis.length > limit) return false;
+    const images = Array.from(node.querySelectorAll('img'));
+    if (images.length > emojis.length) return false;
+    return true;
+  } catch (e) {
+    // Apparently some browsers can't handle `node.textContent.replaceAll`??
+    // If anything in here crashes, skipping it is inconsequential.
+    console.error(e);
+    return false;
+  }
 };
