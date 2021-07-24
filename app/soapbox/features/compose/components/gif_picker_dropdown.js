fbad186629b956e843189ad65bf2f8fa10fc5653
Clovis <portron.cl@gmail.com>
2021-07-24 13:16:27 +0200

    Ajout gif

--- app/soapbox/features/compose/components/gif_picker_dropdown.js
+++ app/soapbox/features/compose/components/gif_picker_dropdown.js
@@ -3,29 +3,61 @@ import PropTypes from 'prop-types';
 
 import TextIconButton from '../components/text_icon_button';
 
-const iconStyle = {
-  height: null,
-  lineHeight: '27px',
-};
+
+class GIFPicker extends React.Component {
+  render() {
+    <div className='compose-form_gif-picker-dropdown_picker'>
+      Bonjour
+    </div>
+  }
+}
 
 export default class GIFPickerDropdown extends React.PureComponent {
 
     state = {
-      expanded: false,
+      active: false,
+      placement: 'bottom'
     }
 
-    handleClick = () => {
+    componentDidMount() {
 
+    }
+
+    open = () => this.setState({ active: true });
+
+    close = () => this.setState({ active: false })
+
+    handleActive = () => {
+      this.setState({
+        active: true,
+        placement: top * 2 < innerHeight ? 'bottom' : 'top'
+      })
+    }
+
+    setTargetRef = c => {
+      this.target = c;
+    }
+  
+    findTarget = () => {
+      return this.target;
     }
 
     render() {
+      const { active, placement } = this.state;
       return (
-        <div className='compose-form_gif-picker-dropdown'>
-          <TextIconButton label='GIF' title='GIF' onClick={this.handleClick} ariaControls='GIF' />
-          <label>
-            <span style={{ display: 'none' }}>GIF</span>
-          </label>
-        </div>
+        <>
+          <div className='compose-form_gif-picker-dropdown' ref={this.setTargetRef}>
+            <TextIconButton label='GIF' title='GIF' onClick={this.handleActive} ariaControls='GIF' />
+            <label>
+              <span style={{ display: 'none' }}>GIF</span>
+            </label>
+          </div>
+          <Overlay show={active} placement={placement} target={this.findTarget}>
+            <GIFPicker
+
+            />
+          </Overlay>
+        </>
       );
     }
 
