075eeee9eee2dbdb60850f3266c8c65db57a9ee3
Alex Gleason <alex@alexgleason.me>
2021-07-25 16:02:38 -0500

    FederationRestrictions: make RestrictedInstance function like an accordion

--- app/soapbox/features/federation_restrictions/components/restricted_instance.js
+++ app/soapbox/features/federation_restrictions/components/restricted_instance.js
@@ -4,6 +4,8 @@ import PropTypes from 'prop-types';
 import ImmutablePureComponent from 'react-immutable-pure-component';
 import { makeGetRemoteInstance } from 'soapbox/selectors';
 import classNames from 'classnames';
+import InstanceRestrictions from './instance_restrictions';
+import Icon from 'soapbox/components/icon';
 
 const getRemoteInstance = makeGetRemoteInstance();
 
@@ -20,12 +22,36 @@ class RestrictedInstance extends ImmutablePureComponent {
     host: PropTypes.string.isRequired,
   }
 
+  state = {
+    expanded: false,
+  }
+
+  toggleExpanded = e => {
+    this.setState({ expanded: !this.state.expanded });
+    e.preventDefault();
+  }
+
   render() {
     const { remoteInstance } = this.props;
+    const { expanded } = this.state;
 
     return (
-      <div className={classNames('restricted-instance', { 'restricted-instance--reject': remoteInstance.getIn(['federation', 'reject']) })}>
-        {remoteInstance.get('host')}
+      <div className={classNames('restricted-instance', {
+        'restricted-instance--reject': remoteInstance.getIn(['federation', 'reject']),
+        'restricted-instance--expanded': expanded,
+      })}
+      >
+        <a href='#' className='restricted-instance__header' onClick={this.toggleExpanded}>
+          <div className='restricted-instance__icon'>
+            <Icon id={expanded ? 'caret-down' : 'caret-right'} />
+          </div>
+          <div className='restricted-instance__host'>
+            {remoteInstance.get('host')}
+          </div>
+        </a>
+        <div className='restricted-instance__restrictions'>
+          <InstanceRestrictions remoteInstance={remoteInstance} />
+        </div>
       </div>
     );
   }
