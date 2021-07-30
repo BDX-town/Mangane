d0f3fe6771fc23a95296cad611ba61605650006e
marcin mikołajczak <git@mkljczk.pl>
2021-07-30 17:51:43 +0200

    Tabbed, paginated search results
    Signed-off-by: marcin mikołajczak <git@mkljczk.pl>

--- app/soapbox/features/compose/components/search_results.js
+++ app/soapbox/features/compose/components/search_results.js
@@ -8,6 +8,9 @@ import ImmutablePureComponent from 'react-immutable-pure-component';
 import Hashtag from '../../../components/hashtag';
 import Icon from 'soapbox/components/icon';
 import LoadingIndicator from 'soapbox/components/loading_indicator';
+import FilterBar from '../../search/components/filter_bar';
+import LoadMore from '../../../components/load_more';
+import classNames from 'classnames';
 
 export default @injectIntl
 class SearchResults extends ImmutablePureComponent {
@@ -15,11 +18,24 @@ class SearchResults extends ImmutablePureComponent {
   static propTypes = {
     results: ImmutablePropTypes.map.isRequired,
     submitted: PropTypes.bool,
+    expandSearch: PropTypes.func.isRequired,
     intl: PropTypes.object.isRequired,
+  };
+
+  state = {
+    selectedFilter: 'accounts',
+  };
+
+  handleLoadMore = () => this.props.expandSearch(this.state.selectedFilter);
+
+  handleSelectFilter = newActiveFilter => {
+    console.log(newActiveFilter);
+    this.setState({ selectedFilter: newActiveFilter });
   };
 
   render() {
     const { results, submitted } = this.props;
+    const { selectedFilter } = this.state;
 
     if (submitted && results.isEmpty()) {
       return (
@@ -29,37 +45,38 @@ class SearchResults extends ImmutablePureComponent {
       );
     }
 
-    let accounts, statuses, hashtags;
+    let searchResults;
     let count = 0;
+    let hasMore = false;
 
-    if (results.get('accounts') && results.get('accounts').size > 0) {
-      count   += results.get('accounts').size;
-      accounts = (
-        <div className='search-results__section'>
-          <h5><Icon id='users' fixedWidth /><FormattedMessage id='search_results.accounts' defaultMessage='People' /></h5>
+    if (selectedFilter === 'accounts' && results.get('accounts') && results.get('accounts').size > 0) {
+      count = results.get('accounts').size;
+      hasMore = results.get('accountsHasMore');
 
+      searchResults = (
+        <div className={classNames('search-results__section', { 'has-more': hasMore })}>
           {results.get('accounts').map(accountId => <AccountContainer key={accountId} id={accountId} />)}
         </div>
       );
     }
 
-    if (results.get('statuses') && results.get('statuses').size > 0) {
-      count   += results.get('statuses').size;
-      statuses = (
-        <div className='search-results__section'>
-          <h5><Icon id='quote-right' fixedWidth /><FormattedMessage id='search_results.statuses' defaultMessage='Posts' /></h5>
+    if (selectedFilter === 'statuses' && results.get('statuses') && results.get('statuses').size > 0) {
+      count = results.get('statuses').size;
+      hasMore = results.get('statusesHasMore');
 
+      searchResults = (
+        <div className={classNames('search-results__section', { 'has-more': hasMore })}>
           {results.get('statuses').map(statusId => <StatusContainer key={statusId} id={statusId} />)}
         </div>
       );
     }
 
-    if (results.get('hashtags') && results.get('hashtags').size > 0) {
-      count += results.get('hashtags').size;
-      hashtags = (
-        <div className='search-results__section'>
-          <h5><Icon id='hashtag' fixedWidth /><FormattedMessage id='search_results.hashtags' defaultMessage='Hashtags' /></h5>
+    if (selectedFilter === 'hashtags' && results.get('hashtags') && results.get('hashtags').size > 0) {
+      count = results.get('hashtags').size;
+      hasMore = results.get('hashtagsHasMore');
 
+      searchResults = (
+        <div className={classNames('search-results__section', { 'has-more': hasMore })}>
           {results.get('hashtags').map(hashtag => <Hashtag key={hashtag.get('name')} hashtag={hashtag} />)}
         </div>
       );
@@ -72,9 +89,11 @@ class SearchResults extends ImmutablePureComponent {
           <FormattedMessage id='search_results.total' defaultMessage='{count, number} {count, plural, one {result} other {results}}' values={{ count }} />
         </div>
 
-        {accounts}
-        {statuses}
-        {hashtags}
+        <FilterBar selectedFilter={selectedFilter} selectFilter={this.handleSelectFilter} />
+
+        {searchResults}
+
+        {hasMore && <LoadMore visible onClick={this.handleLoadMore} />}
       </div>
     );
   }
--- app/soapbox/features/compose/containers/search_results_container.js
+++ app/soapbox/features/compose/containers/search_results_container.js
@@ -1,15 +1,19 @@
 import { connect } from 'react-redux';
 import SearchResults from '../components/search_results';
 import { fetchSuggestions, dismissSuggestion } from '../../../actions/suggestions';
+import { expandSearch } from '../../../actions/search';
 
-const mapStateToProps = state => ({
-  results: state.getIn(['search', 'results']),
-  suggestions: state.getIn(['suggestions', 'items']),
-  submitted: state.getIn(['search', 'submitted']),
-});
+const mapStateToProps = state => {
+  return {
+    results: state.getIn(['search', 'results']),
+    suggestions: state.getIn(['suggestions', 'items']),
+    submitted: state.getIn(['search', 'submitted']),
+  };
+};
 
 const mapDispatchToProps = dispatch => ({
   fetchSuggestions: () => dispatch(fetchSuggestions()),
+  expandSearch: type => dispatch(expandSearch(type)),
   dismissSuggestion: account => dispatch(dismissSuggestion(account.get('id'))),
 });
 
