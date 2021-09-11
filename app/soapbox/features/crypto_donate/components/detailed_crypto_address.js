5166a71c270107861ab673595ba8e15ac4ef6922
Alex Gleason <alex@alexgleason.me>
2021-09-11 16:21:12 -0500

    CryptoDonate: refactor CryptoIcon into its own component

--- app/soapbox/features/crypto_donate/components/detailed_crypto_address.js
+++ app/soapbox/features/crypto_donate/components/detailed_crypto_address.js
@@ -5,7 +5,7 @@ import ImmutablePureComponent from 'react-immutable-pure-component';
 import Icon from 'soapbox/components/icon';
 import QRCode from 'qrcode.react';
 import CoinDB from '../utils/coin_db';
-import { getCoinIcon } from '../utils/coin_icons';
+import CryptoIcon from './crypto_icon';
 import { CopyableInput } from 'soapbox/features/forms';
 import { getExplorerUrl } from '../utils/block_explorer';
 
@@ -26,9 +26,11 @@ class DetailedCryptoAddress extends ImmutablePureComponent {
     return (
       <div className='crypto-address'>
         <div className='crypto-address__head'>
-          <div className='crypto-address__icon'>
-            <img src={getCoinIcon(ticker)} alt={title} />
-          </div>
+          <CryptoIcon
+            className='crypto-address__icon'
+            ticker={ticker}
+            title={title}
+          />
           <div className='crypto-address__title'>{title || ticker.toUpperCase()}</div>
           <div className='crypto-address__actions'>
             {explorerUrl && <a href={explorerUrl} target='_blank'>
