d040de4c15e718b1b70f86ae18f3c298a4d81581
Alex Gleason <alex@alexgleason.me>
2021-09-11 17:03:53 -0500

    Remove wdyr

--- app/soapbox/wdyr.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { NODE_ENV } from 'soapbox/build_config';
-
-if (NODE_ENV === 'development') {
-  const whyDidYouRender = require('@welldone-software/why-did-you-render');
-  whyDidYouRender(React);
-}
