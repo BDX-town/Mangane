cb23506c49beeb20afc12f13735ec8426be0571b
Alex Gleason <alex@alexgleason.me>
2021-09-16 11:04:42 -0500

    Properly upgrade axios, redux, react-redux, @gamestdio/websocket, and react-swipeable-views. Fixes redux.

--- webpack/rules/index.js
+++ webpack/rules/index.js
@@ -6,21 +6,12 @@ const css = require('./css');
 const assets = require('./assets');
 const nodeModules = require('./node_modules');
 
-// https://github.com/webpack/webpack/issues/11467
-const disableFullySpecified = {
-  test: /\.m?js/,
-  resolve: {
-    fullySpecified: false,
-  },
-};
-
 // Webpack loaders are processed in reverse order
 // https://webpack.js.org/concepts/loaders/#loader-features
 // Lastly, process static files using file loader
 module.exports = [
   ...assets,
   css,
-  disableFullySpecified,
   nodeModules,
   babel,
   git,
