25075074ff213bc9394fdb11f208b71323bcd4d6
Alex Gleason <alex@alexgleason.me>
2021-09-17 11:26:57 -0500

    webpack/index.js --> webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ switch(NODE_ENV) {
 case 'development':
 case 'production':
 case 'test':
-  module.exports = require(`./${NODE_ENV}`); break;
+  module.exports = require(`./webpack/${NODE_ENV}`); break;
 default:
   console.error('ERROR: NODE_ENV must be set to either `development`, `test`, or `production`.');
   process.exit(1);
