1be73d13a1ae4faca62713ee5d4d83e873d91813
Alex Gleason <alex@alexgleason.me>
2021-10-02 15:18:27 -0500

    Suggestions: fix optimistic dismissal

--- app/soapbox/reducers/__tests__/suggestions-test.js
+++ app/soapbox/reducers/__tests__/suggestions-test.js
@@ -1,5 +1,6 @@
 import reducer from '../suggestions';
-import { Map as ImmutableMap, List as ImmutableList } from 'immutable';
+import { Map as ImmutableMap, List as ImmutableList, fromJS } from 'immutable';
+import { SUGGESTIONS_DISMISS } from 'soapbox/actions/suggestions';
 
 describe('suggestions reducer', () => {
   it('should return the initial state', () => {
@@ -7,5 +8,30 @@ describe('suggestions reducer', () => {
       items: ImmutableList(),
       isLoading: false,
     }));
+  });
+
+  describe('SUGGESTIONS_DISMISS', () => {
+    it('should remove the account', () => {
+      const action = { type: SUGGESTIONS_DISMISS, id: '123' };
+
+      const state = fromJS({
+        items: [
+          { account: '123', source: 'past_interactions' },
+          { account: '456', source: 'past_interactions' },
+          { account: '789', source: 'past_interactions' },
+        ],
+        isLoading: false,
+      });
+
+      const expected = fromJS({
+        items: [
+          { account: '456', source: 'past_interactions' },
+          { account: '789', source: 'past_interactions' },
+        ],
+        isLoading: false,
+      });
+
+      expect(reducer(state, action)).toEqual(expected);
+    });
   });
 });
