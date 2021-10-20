5a19d5261780b1834c84602fba888cb728d07669
Alex Gleason <alex@alexgleason.me>
2021-10-20 17:50:03 -0500

    errorsMiddleware: only alert when the error has a response (eg not when offline)

--- app/soapbox/middleware/errors.js
+++ app/soapbox/middleware/errors.js
@@ -3,8 +3,10 @@ import { showAlertForError } from '../actions/alerts';
 const isFailType = type => type.endsWith('_FAIL');
 const isRememberFailType = type => type.endsWith('_REMEMBER_FAIL');
 
-const shouldShowError = ({ type, skipAlert }) => {
-  return !skipAlert && isFailType(type) && !isRememberFailType(type);
+const hasResponse = error => Boolean(error && error.response);
+
+const shouldShowError = ({ type, skipAlert, error }) => {
+  return !skipAlert && hasResponse(error) && isFailType(type) && !isRememberFailType(type);
 };
 
 export default function errorsMiddleware() {
