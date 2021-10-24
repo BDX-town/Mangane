f2b91a22318e2b66d4071b3cc0f2d094fbe9d16f
marcin mikołajczak <git@mkljczk.pl>
2021-10-24 14:03:42 +0200

    Fix polls for unauthenticated users
    Signed-off-by: marcin mikołajczak <git@mkljczk.pl>

--- app/soapbox/containers/poll_container.js
+++ app/soapbox/containers/poll_container.js
@@ -3,6 +3,7 @@ import Poll from 'soapbox/components/poll';
 
 const mapStateToProps = (state, { pollId }) => ({
   poll: state.getIn(['polls', pollId]),
+  me: state.get('me'),
 });
 
 export default connect(mapStateToProps)(Poll);
