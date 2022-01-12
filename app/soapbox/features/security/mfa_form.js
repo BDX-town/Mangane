f675e7992a9ca0dc5888370920e0f29fa0e50d56
Alex Gleason <alex@alexgleason.me>
2022-01-12 12:03:24 -0600

    MfaForm: improve styles and usability

--- app/soapbox/features/security/mfa_form.js
+++ app/soapbox/features/security/mfa_form.js
@@ -47,6 +47,8 @@ const messages = defineMessages({
   disableFail: { id: 'security.disable.fail', defaultMessage: 'Incorrect password. Try again.' },
   mfaDisableSuccess: { id: 'mfa.disable.success_message', defaultMessage: 'MFA disabled' },
   mfaConfirmSuccess: { id: 'mfa.confirm.success_message', defaultMessage: 'MFA confirmed' },
+  codePlaceholder: { id: 'mfa.mfa_setup.code_placeholder', defaultMessage: 'Code' },
+  passwordPlaceholder: { id: 'mfa.mfa_setup.password_placeholder', defaultMessage: 'Password' },
 });
 
 const mapStateToProps = state => ({
@@ -147,26 +149,33 @@ class DisableOtpForm extends ImmutablePureComponent {
     const { isLoading, password } = this.state;
 
     return (
-      <SimpleForm onSubmit={this.handleSubmit} disabled={isLoading}>
-        <div className='security-settings-panel'>
+      <div className='security-settings-panel'>
+        <SimpleForm onSubmit={this.handleSubmit} disabled={isLoading}>
           <h1 className='security-settings-panel__setup-otp'>
             <FormattedMessage id='mfa.otp_enabled_title' defaultMessage='OTP Enabled' />
           </h1>
-          <div><FormattedMessage id='mfa.otp_enabled_description' defaultMessage='You have enabled two-factor authentication via OTP.' /></div>
-          <div><FormattedMessage id='mfa.mfa_disable_enter_password' defaultMessage='Enter your current password to disable two-factor auth:' /></div>
+          <h2 className='security-settings-panel__setup-otp'>
+            <FormattedMessage id='mfa.otp_enabled_description' defaultMessage='You have enabled two-factor authentication via OTP.' />
+          </h2>
           <ShowablePassword
+            label={intl.formatMessage(messages.passwordPlaceholder)}
+            placeholder={intl.formatMessage(messages.passwordPlaceholder)}
+            hint={<FormattedMessage id='mfa.mfa_disable_enter_password' defaultMessage='Enter your current password to disable two-factor auth.' />}
             disabled={isLoading}
             name='password'
             onChange={this.handleInputChange}
             value={password}
+            required
           />
-          <Button
-            disabled={isLoading}
-            className='button button-primary disable'
-            text={intl.formatMessage(messages.mfa_setup_disable_button)}
-          />
-        </div>
-      </SimpleForm>
+          <div className='security-settings-panel__setup-otp__buttons'>
+            <Button
+              disabled={isLoading}
+              className='button button-primary disable'
+              text={intl.formatMessage(messages.mfa_setup_disable_button)}
+            />
+          </div>
+        </SimpleForm>
+      </div>
     );
   }
 
@@ -202,6 +211,7 @@ class EnableOtpForm extends ImmutablePureComponent {
 
   handleCancelClick = e => {
     this.context.router.history.push('../auth/edit');
+    e.preventDefault();
   }
 
   render() {
@@ -209,8 +219,8 @@ class EnableOtpForm extends ImmutablePureComponent {
     const { backupCodes, displayOtpForm } = this.state;
 
     return (
-      <SimpleForm>
-        <div className='security-settings-panel'>
+      <div className='security-settings-panel'>
+        <SimpleForm>
           <h1 className='security-settings-panel__setup-otp'>
             <FormattedMessage id='mfa.setup_otp_title' defaultMessage='OTP Disabled' />
           </h1>
@@ -244,8 +254,8 @@ class EnableOtpForm extends ImmutablePureComponent {
               )}
             </div>
           )}
-        </div>
-      </SimpleForm>
+        </SimpleForm>
+      </div>
     );
   }
 
@@ -286,6 +296,11 @@ class OtpConfirmForm extends ImmutablePureComponent {
     this.setState({ [e.target.name]: e.target.value });
   }
 
+  handleCancelClick = e => {
+    this.context.router.history.push('../auth/edit');
+    e.preventDefault();
+  }
+
   handleSubmit = e => {
     const { dispatch, intl } = this.props;
     const { code, password } = this.state;
@@ -308,41 +323,48 @@ class OtpConfirmForm extends ImmutablePureComponent {
     const { isLoading, qrCodeURI, confirm_key, password, code } = this.state;
 
     return (
-      <SimpleForm onSubmit={this.handleSubmit} disabled={isLoading}>
-        <div className='security-settings-panel'>
+      <div className='security-settings-panel'>
+        <SimpleForm onSubmit={this.handleSubmit} disabled={isLoading}>
 
           <fieldset disabled={false}>
             <FieldsGroup>
               <div className='security-settings-panel__section-container'>
                 <h2><FormattedMessage id='mfa.mfa_setup_scan_title' defaultMessage='Scan' /></h2>
 
-                <div><FormattedMessage id='mfa.mfa_setup_scan_description' defaultMessage='Using your two-factor app, scan this QR code or enter text key:' /></div>
+                <div><FormattedMessage id='mfa.mfa_setup_scan_description' defaultMessage='Using your two-factor app, scan this QR code or enter the text key.' /></div>
 
-                <span className='security-settings-panel qr-code'>
+                <div className='security-settings-panel__qr-code'>
                   <QRCode value={qrCodeURI} />
-                </span>
+                  <div className='security-settings-panel__confirm-key'>
+                    {confirm_key}
+                  </div>
+                </div>
 
-                <div className='security-settings-panel confirm-key'><FormattedMessage id='mfa.mfa_setup_scan_key' defaultMessage='Key:' /> {confirm_key}</div>
               </div>
 
               <div className='security-settings-panel__section-container'>
                 <h2><FormattedMessage id='mfa.mfa_setup_verify_title' defaultMessage='Verify' /></h2>
 
-                <div><FormattedMessage id='mfa.mfa_setup_verify_description' defaultMessage='To enable two-factor authentication, enter the code from your two-factor app:' /></div>
                 <TextInput
                   name='code'
+                  label={intl.formatMessage(messages.codePlaceholder)}
+                  hint={<FormattedMessage id='mfa.mfa_setup.code_hint' defaultMessage='Enter the code from your two-factor app.' />}
+                  placeholder={intl.formatMessage(messages.codePlaceholder)}
                   onChange={this.handleInputChange}
                   autoComplete='off'
                   value={code}
                   disabled={isLoading}
+                  required
                 />
-
-                <div><FormattedMessage id='mfa.mfa_setup_enter_password' defaultMessage='Enter your current password to confirm your identity:' /></div>
                 <ShowablePassword
                   name='password'
+                  label={intl.formatMessage(messages.passwordPlaceholder)}
+                  hint={<FormattedMessage id='mfa.mfa_setup.password_hint' defaultMessage='Enter your current password to confirm your identity.' />}
+                  placeholder={intl.formatMessage(messages.passwordPlaceholder)}
                   onChange={this.handleInputChange}
                   value={password}
                   disabled={isLoading}
+                  required
                 />
               </div>
             </FieldsGroup>
@@ -362,8 +384,8 @@ class OtpConfirmForm extends ImmutablePureComponent {
               disabled={isLoading}
             />
           </div>
-        </div>
-      </SimpleForm>
+        </SimpleForm>
+      </div>
     );
   }
 
