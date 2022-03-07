1c910248bf382b0fbd77d17eeada0dd240d9f1c3
Alex Gleason <alex@alexgleason.me>
2022-03-07 16:54:45 -0600

    Set Tailwind colors with CSS variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,38 @@
+// https://tailwindcss.com/docs/customizing-colors#using-css-variables
+function withOpacityValue(variable) {
+  return ({ opacityValue }) => {
+    if (opacityValue === undefined) {
+      return `rgb(var(${variable}))`;
+    }
+    return `rgb(var(${variable}) / ${opacityValue})`;
+  };
+}
+
+// Parse list of shades into Tailwind function with CSS variables
+const parseShades = (color, shades) => {
+  return shades.reduce((obj, shade) => {
+    obj[shade] = withOpacityValue(`--color-${color}-${shade}`);
+    return obj;
+  }, {});
+};
+
+// Parse color matrix into Tailwind colors object
+const parseColors = colors => {
+  return Object.keys(colors).reduce((obj, color) => {
+    obj[color] = parseShades(color, colors[color]);
+    return obj;
+  }, {});
+};
+
+// Define color matrix (of available colors)
+const colors = {
+  gray: [50, 100, 200, 300, 400, 500, 600, 700, 800, 900],
+  primary: [50, 100, 200, 300, 400, 500, 600, 700, 800, 900],
+  success: [50, 100, 200, 300, 400, 500, 600, 700, 800, 900],
+  danger: [50, 100, 200, 300, 400, 500, 600, 700, 800, 900],
+  accent: [300, 500],
+};
+
 module.exports = {
   content: ['./app/**/*.{html,js,ts,tsx}'],
   darkMode: 'class',
@@ -31,63 +66,11 @@ module.exports = {
           'Noto Color Emoji',
         ],
       },
-      colors: {
-        gray: {
-          50: '#f9fafb',
-          100: '#f3f4f6',
-          200: '#e5e7eb',
-          300: '#d1d5db',
-          400: '#9ca3af',
-          500: '#6b7280',
-          600: '#4b5563',
-          700: '#374151',
-          800: '#1f2937',
-          900: '#111827',
-        },
-        primary: {
-          50: '#eef2ff',
-          100: '#e0e7ff',
-          200: '#c7d2fe',
-          300: '#a5b4fc',
-          400: '#818cf8',
-          500: '#6366f1',
-          600: '#5448ee',
-          700: '#4338ca',
-          800: '#3730a3',
-          900: '#312e81',
-        },
-        success: {
-          50: '#f0fdf4',
-          100: '#dcfce7',
-          200: '#bbf7d0',
-          300: '#86efac',
-          400: '#4ade80',
-          500: '#22c55e',
-          600: '#16a34a',
-          700: '#15803d',
-          800: '#166534',
-          900: '#14532d',
-        },
-        danger: {
-          50: '#fef2f2',
-          100: '#fee2e2',
-          200: '#fecaca',
-          300: '#fca5a5',
-          400: '#f87171',
-          500: '#ef4444',
-          600: '#dc2626',
-          700: '#b91c1c',
-          800: '#991b1b',
-          900: '#7f1d1d',
-        },
-        accent: {
-          300: '#ff5f87',
-          500: '#ff4775',
-        },
-        'gradient-purple': '#b8a3f9',
-        'gradient-blue': '#9bd5ff',
-        'sea-blue': '#2feecc',
-      },
+      colors: Object.assign(parseColors(colors), {
+        'gradient-purple': withOpacityValue('--color-gradient-purple'),
+        'gradient-blue': withOpacityValue('--color-gradient-blue'),
+        'sea-blue': withOpacityValue('--color-sea-blue'),
+      }),
     },
   },
   plugins: [
