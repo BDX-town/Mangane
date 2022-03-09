6e61cb525c405ca41df37c411a7b0bd1c0b414f1
Alex Gleason <alex@alexgleason.me>
2022-03-09 13:26:48 -0600

    Move Account interface to types/ directory

--- app/soapbox/normalizers/account.ts
+++ app/soapbox/normalizers/account.ts
@@ -1,44 +1,7 @@
 import { Map as ImmutableMap, List as ImmutableList, Record } from 'immutable';
 
+import { IAccount } from 'soapbox/types';
 import { mergeDefined } from 'soapbox/utils/normalizers';
-
-interface Account {
-  acct: string;
-  avatar: string;
-  avatar_static: string;
-  birthday: Date | undefined;
-  bot: boolean;
-  created_at: Date;
-  display_name: string;
-  emojis: ImmutableList<any>;
-  fields: ImmutableList<any>;
-  followers_count: number;
-  following_count: number;
-  fqn: string;
-  header: string;
-  header_static: string;
-  id: string;
-  last_status_at: Date;
-  location: string;
-  locked: boolean;
-  moved: null;
-  note: string;
-  pleroma: ImmutableMap<any, any>;
-  source: ImmutableMap<any, any>;
-  statuses_count: number;
-  uri: string;
-  url: string;
-  username: string;
-  verified: boolean;
-
-  // Internal fields
-  display_name_html: string;
-  note_emojified: string;
-  note_plain: string;
-  patron: ImmutableMap<any, any>;
-  relationship: ImmutableList<any>;
-  should_refetch: boolean;
-}
 
 const AccountRecord = Record({
   acct: '',
@@ -130,7 +93,7 @@ const normalizeLocation = (account: ImmutableMap<string, any>) => {
   });
 };
 
-export const normalizeAccount = (account: ImmutableMap<string, any>): Account => {
+export const normalizeAccount = (account: ImmutableMap<string, any>): IAccount => {
   return AccountRecord(
     account.withMutations(account => {
       normalizePleromaLegacyFields(account);
---
+++ app/soapbox/types/index.ts
@@ -0,0 +1,3 @@
+import { IAccount } from './account';
+
+export { IAccount };
