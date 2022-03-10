71ea3ed425e1acd86642b9f2025c7dad10ac4aba
marcin mikołajczak <git@mkljczk.pl>
2022-03-10 18:47:20 +0100

    Include media attachments when redrafting on Mastodon
    Signed-off-by: marcin mikołajczak <git@mkljczk.pl>

--- app/soapbox/utils/features.js
+++ app/soapbox/utils/features.js
@@ -110,6 +110,7 @@ export const getFeatures = createSelector([instance => instance], instance => {
       v.software === MASTODON && gte(v.compatVersion, '3.2.0'),
       v.software === PLEROMA && gte(v.version, '2.4.50'),
     ]),
+    redraftMedia: v.software === MASTODON,
   }, overrides);
 });
 
