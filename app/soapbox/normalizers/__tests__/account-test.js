b46f31e9f4786543e61b73d078e6026fefb48963
Alex Gleason <alex@alexgleason.me>
2022-03-12 14:19:34 -0600

    Account normalizer emojification tests

--- app/soapbox/normalizers/__tests__/account-test.js
+++ app/soapbox/normalizers/__tests__/account-test.js
@@ -4,6 +4,8 @@ import { normalizeAccount } from '../account';
 
 describe('normalizeAccount()', () => {
   it('normalizes a mention', () => {
+    const avatarMissing = require('images/avatar-missing.png');
+
     const mention = fromJS({
       acct: 'NEETzsche@iddqd.social',
       id: '9v5bw7hEGBPc9nrpzc',
@@ -14,6 +16,8 @@ describe('normalizeAccount()', () => {
     const result = normalizeAccount(mention);
     expect(result.emojis).toEqual(fromJS([]));
     expect(result.display_name).toEqual('NEETzsche');
+    expect(result.avatar).toEqual(avatarMissing);
+    expect(result.avatar_static).toEqual(avatarMissing);
     expect(result.verified).toBe(false);
   });
 
@@ -21,14 +25,14 @@ describe('normalizeAccount()', () => {
     const account = fromJS(require('soapbox/__fixtures__/fedibird-account.json'));
     const result = normalizeAccount(account);
 
-    expect(result.get('birthday')).toEqual('1993-07-03');
+    expect(result.birthday).toEqual('1993-07-03');
   });
 
   it('normalizes Pleroma birthday', () => {
     const account = fromJS(require('soapbox/__fixtures__/pleroma-account.json'));
     const result = normalizeAccount(account);
 
-    expect(result.get('birthday')).toEqual('1993-07-03');
+    expect(result.birthday).toEqual('1993-07-03');
   });
 
   it('normalizes Pleroma legacy fields', () => {
@@ -54,30 +58,79 @@ describe('normalizeAccount()', () => {
   it('normalizes a verified Pleroma user', () => {
     const account = fromJS(require('soapbox/__fixtures__/mk.json'));
     const result = normalizeAccount(account);
-    expect(result.get('verified')).toBe(true);
+    expect(result.verified).toBe(true);
   });
 
   it('normalizes an unverified Pleroma user', () => {
     const account = fromJS(require('soapbox/__fixtures__/pleroma-account.json'));
     const result = normalizeAccount(account);
-    expect(result.get('verified')).toBe(false);
+    expect(result.verified).toBe(false);
   });
 
   it('normalizes a verified Truth Social user', () => {
     const account = fromJS(require('soapbox/__fixtures__/realDonaldTrump.json'));
     const result = normalizeAccount(account);
-    expect(result.get('verified')).toBe(true);
+    expect(result.verified).toBe(true);
   });
 
   it('normalizes Fedibird location', () => {
     const account = fromJS(require('soapbox/__fixtures__/fedibird-account.json'));
     const result = normalizeAccount(account);
-    expect(result.get('location')).toBe('Texas, USA');
+    expect(result.location).toBe('Texas, USA');
   });
 
   it('normalizes Truth Social location', () => {
     const account = fromJS(require('soapbox/__fixtures__/truthsocial-account.json'));
     const result = normalizeAccount(account);
-    expect(result.get('location')).toBe('Texas');
+    expect(result.location).toBe('Texas');
+  });
+
+  it('sets display_name from username', () => {
+    const account = fromJS({ username: 'alex' });
+    const result = normalizeAccount(account);
+    expect(result.display_name).toBe('alex');
+  });
+
+  it('sets display_name from acct', () => {
+    const account = fromJS({ acct: 'alex@gleasonator.com' });
+    const result = normalizeAccount(account);
+    expect(result.display_name).toBe('alex');
+  });
+
+  it('overrides a whitespace display_name', () => {
+    const account = fromJS({ username: 'alex', display_name: ' ' });
+    const result = normalizeAccount(account);
+    expect(result.display_name).toBe('alex');
+  });
+
+  it('emojifies display name as `display_name_html`', () => {
+    const account = fromJS(require('soapbox/__fixtures__/account-with-emojis.json'));
+    const result = normalizeAccount(account);
+    const expected = 'Alex Gleason <img draggable="false" class="emojione" alt="😂" title=":joy:" src="/packs/emoji/1f602.svg" /> <img draggable="false" class="emojione" alt=":soapbox:" title=":soapbox:" src="https://gleasonator.com/emoji/Gleasonator/soapbox.png" /> <img draggable="false" class="emojione" alt=":ablobcatrainbow:" title=":ablobcatrainbow:" src="https://gleasonator.com/emoji/blobcat/ablobcatrainbow.png" />';
+    expect(result.display_name_html).toBe(expected);
+  });
+
+  it('emojifies note as `note_emojified`', () => {
+    const account = fromJS(require('soapbox/__fixtures__/account-with-emojis.json'));
+    const result = normalizeAccount(account);
+    const expected = 'I create Fediverse software that empowers people online. <img draggable="false" class="emojione" alt=":soapbox:" title=":soapbox:" src="https://gleasonator.com/emoji/Gleasonator/soapbox.png" /><br/><br/>I&#39;m vegan btw<br/><br/>Note: If you have a question for me, please tag me publicly. This gives the opportunity for others to chime in, and bystanders to learn.';
+    expect(result.note_emojified).toBe(expected);
+  });
+
+  it('unescapes HTML note as `note_plain`', () => {
+    const account = fromJS(require('soapbox/__fixtures__/account-with-emojis.json'));
+    const result = normalizeAccount(account);
+    const expected = 'I create Fediverse software that empowers people online. :soapbox:\n\nI\'m vegan btw\n\nNote: If you have a question for me, please tag me publicly. This gives the opportunity for others to chime in, and bystanders to learn.';
+    expect(result.note_plain).toBe(expected);
+  });
+
+  it('emojifies custom profile field', () => {
+    const account = fromJS(require('soapbox/__fixtures__/account-with-emojis.json'));
+    const result = normalizeAccount(account);
+    const field = result.fields.get(1);
+
+    expect(field.name_emojified).toBe('Soapbox <img draggable="false" class="emojione" alt=":ablobcatrainbow:" title=":ablobcatrainbow:" src="https://gleasonator.com/emoji/blobcat/ablobcatrainbow.png" />');
+    expect(field.value_emojified).toBe('<a href="https://soapbox.pub" rel="ugc">https://soapbox.pub</a> <img draggable="false" class="emojione" alt=":soapbox:" title=":soapbox:" src="https://gleasonator.com/emoji/Gleasonator/soapbox.png" />');
+    expect(field.value_plain).toBe('https://soapbox.pub :soapbox:');
   });
 });
