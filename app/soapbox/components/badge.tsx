c420e83bffe3ba0c6a7f5103a508e1dc48a07c17
Alex Gleason <alex@alexgleason.me>
2022-03-18 15:35:22 -0500

    Throw out accidental badge.tsx changes

--- app/soapbox/components/badge.tsx
+++ app/soapbox/components/badge.tsx
@@ -1,15 +1,9 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-import { useAppSelector } from 'soapbox/hooks';
-
-const Badge = (props: any) => {
-  const title = useAppSelector(state => state.instance.titles);
-
-  return (
-    <span title={title} className={'badge badge--' + props.slug}>{props.title}</span>
-  );
-};
+const Badge = (props: any) => (
+  <span className={'badge badge--' + props.slug}>{props.title}</span>
+);
 
 Badge.propTypes = {
   title: PropTypes.string.isRequired,
