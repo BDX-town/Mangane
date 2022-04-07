182f5e6302b3c966e2aff5f68d3e0b7b05e0e3e1
Alex Gleason <alex@alexgleason.me>
2022-04-07 10:24:41 -0500

    Refactor Emoji component, add more comprehensive tests

--- app/soapbox/components/ui/emoji/__tests__/emoji.test.tsx
+++ app/soapbox/components/ui/emoji/__tests__/emoji.test.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import * as React from 'react';
 
 import { render, screen } from '../../../../jest/test-helpers';
 import Emoji from '../emoji';
 
 describe('<Emoji />', () => {
-  it('renders the given text', () => {
-    render(<Emoji emoji='smile' />);
+  it('renders a simple emoji', () => {
+    render(<Emoji emoji='😀' />);
 
-    expect(screen.getByRole('img').getAttribute('alt')).toBe('smile');
+    const img = screen.getByRole('img');
+    expect(img.getAttribute('src')).toBe('/packs/emoji/1f600.svg');
+    expect(img.getAttribute('alt')).toBe('😀');
+  });
+
+  // https://emojipedia.org/emoji-flag-sequence/
+  it('renders a sequence emoji', () => {
+    render(<Emoji emoji='🇺🇸' />);
+
+    const img = screen.getByRole('img');
+    expect(img.getAttribute('src')).toBe('/packs/emoji/1f1fa-1f1f8.svg');
+    expect(img.getAttribute('alt')).toBe('🇺🇸');
   });
 });
