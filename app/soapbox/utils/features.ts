bc8e9254cf4b748dcd9d3e256cc032f392e6177a
Alex Gleason <alex@alexgleason.me>
2022-04-16 21:16:01 -0500

    EditProfile: conditional features

--- app/soapbox/utils/features.ts
+++ app/soapbox/utils/features.ts
@@ -142,6 +142,8 @@ const getInstanceFeatures = (instance: Instance) => {
     trendingTruths: v.software === TRUTHSOCIAL,
     trendingStatuses: v.software === MASTODON && gte(v.compatVersion, '3.5.0'),
     pepe: v.software === TRUTHSOCIAL,
+    accountLocation: v.software === TRUTHSOCIAL,
+    accountWebsite: v.software === TRUTHSOCIAL,
 
     // FIXME: long-term this shouldn't be a feature,
     // but for now we want it to be overrideable in the build
