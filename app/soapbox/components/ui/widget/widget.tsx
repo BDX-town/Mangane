97b5c5af43776adb007fddc7768abde5f81eafb2
Alex Gleason <alex@alexgleason.me>
2022-04-18 22:49:17 -0500

    Widget: add actions

--- app/soapbox/components/ui/widget/widget.tsx
+++ app/soapbox/components/ui/widget/widget.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-import { Stack, Text } from 'soapbox/components/ui';
+import { Stack, HStack, Text, IconButton } from 'soapbox/components/ui';
 
 interface IWidgetTitle {
-  title: string | React.ReactNode
+  title: string | React.ReactNode,
 }
 
 const WidgetTitle = ({ title }: IWidgetTitle): JSX.Element => (
@@ -16,12 +16,31 @@ const WidgetBody: React.FC = ({ children }): JSX.Element => (
 
 interface IWidget {
   title: string | React.ReactNode,
+  onActionClick?: () => void,
+  actionIcon?: string,
+  actionTitle?: string,
 }
 
-const Widget: React.FC<IWidget> = ({ title, children }): JSX.Element => {
+const Widget: React.FC<IWidget> = ({
+  title,
+  children,
+  onActionClick,
+  actionIcon = require('@tabler/icons/icons/arrow-right.svg'),
+  actionTitle,
+}): JSX.Element => {
   return (
     <Stack space={2}>
-      <WidgetTitle title={title} />
+      <HStack alignItems='center'>
+        <WidgetTitle title={title} />
+        {onActionClick && (
+          <IconButton
+            className='w-6 h-6 ml-2'
+            src={actionIcon}
+            onClick={onActionClick}
+            title={actionTitle}
+          />
+        )}
+      </HStack>
       <WidgetBody>{children}</WidgetBody>
     </Stack>
   );
