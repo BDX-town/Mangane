f476b5ce96fddf98ae81c423463a1005ba859fe7
Alex Gleason <alex@alexgleason.me>
2022-04-21 17:03:29 -0500

    Mock IndexedDB in tests

--- app/soapbox/jest/test-setup.ts
+++ app/soapbox/jest/test-setup.ts
@@ -6,6 +6,10 @@ import { __clear as clearApiMocks } from '../__mocks__/api';
 jest.mock('soapbox/api');
 afterEach(() => clearApiMocks());
 
+// Mock IndexedDB
+// https://dev.to/andyhaskell/testing-your-indexeddb-code-with-jest-2o17
+require('fake-indexeddb/auto');
+
 // Mock external dependencies
 jest.mock('uuid', () => ({ v4: jest.fn(() => '1') }));
 
