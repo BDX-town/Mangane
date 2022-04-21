b6c7e3717b92801331949c25602d89cf7fad2ec4
Alex Gleason <alex@alexgleason.me>
2022-04-21 12:24:54 -0500

    test-setup: mock uuid as a string

--- app/soapbox/jest/test-setup.ts
+++ app/soapbox/jest/test-setup.ts
@@ -7,7 +7,7 @@ jest.mock('soapbox/api');
 afterEach(() => clearApiMocks());
 
 // Mock external dependencies
-jest.mock('uuid', () => ({ v4: jest.fn(() => 1) }));
+jest.mock('uuid', () => ({ v4: jest.fn(() => '1') }));
 
 const intersectionObserverMock = () => ({ observe: () => null, disconnect: () => null });
 window.IntersectionObserver = jest.fn().mockImplementation(intersectionObserverMock);
