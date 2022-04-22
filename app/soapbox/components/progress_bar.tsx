8f848877a039a283744fa670fee25ba17a26577e
marcin mikołajczak <git@mkljczk.pl>
2022-04-22 19:08:37 +0200

    yarn lint:js
    Signed-off-by: marcin mikołajczak <git@mkljczk.pl>

--- app/soapbox/components/progress_bar.tsx
+++ app/soapbox/components/progress_bar.tsx
@@ -8,6 +8,6 @@ const ProgressBar: React.FC<IProgressBar> = ({ progress }) => (
   <div className='progress-bar'>
     <div className='progress-bar__progress' style={{ width: `${Math.floor(progress*100)}%` }} />
   </div>
-)
+);
 
 export default ProgressBar;
