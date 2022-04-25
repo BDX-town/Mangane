c6456a43b61931809fe0978f40ff68d60e2481bf
Alex Gleason <alex@alexgleason.me>
2022-04-25 13:57:24 -0500

    Convert actions/instance to use createAsyncThunk()

--- app/soapbox/reducers/meta.ts
+++ app/soapbox/reducers/meta.ts
@@ -2,7 +2,7 @@
 
 import { Record as ImmutableRecord } from 'immutable';
 
-import { INSTANCE_FETCH_FAIL } from 'soapbox/actions/instance';
+import { fetchInstance } from 'soapbox/actions/instance';
 
 import type { AnyAction } from 'redux';
 
@@ -12,7 +12,7 @@ const ReducerRecord = ImmutableRecord({
 
 export default function meta(state = ReducerRecord(), action: AnyAction) {
   switch(action.type) {
-  case INSTANCE_FETCH_FAIL:
+  case fetchInstance.rejected.toString():
     return state.set('instance_fetch_failed', true);
   default:
     return state;
