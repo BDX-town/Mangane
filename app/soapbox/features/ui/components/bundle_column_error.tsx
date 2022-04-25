3788980856759b587baa1e10dd2ede2eb7c3c92b
Alex Gleason <alex@alexgleason.me>
2022-04-25 18:04:00 -0500

    BundleColumnError: improve styles

--- app/soapbox/features/ui/components/bundle_column_error.tsx
+++ app/soapbox/features/ui/components/bundle_column_error.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import { defineMessages, useIntl } from 'react-intl';
 
-import IconButton from '../../../components/icon_button';
-
-import Column from './column';
-import ColumnHeader from './column_header';
+import { Column, Stack, Text, IconButton } from 'soapbox/components/ui';
 
 const messages = defineMessages({
   title: { id: 'bundle_column_error.title', defaultMessage: 'Network error' },
-  body: { id: 'bundle_column_error.body', defaultMessage: 'Something went wrong while loading this component.' },
+  body: { id: 'bundle_column_error.body', defaultMessage: 'Something went wrong while loading this page.' },
   retry: { id: 'bundle_column_error.retry', defaultMessage: 'Try again' },
 });
 
@@ -24,12 +21,17 @@ const BundleColumnError: React.FC<IBundleColumnError> = ({ onRetry }) => {
   };
 
   return (
-    <Column>
-      <ColumnHeader icon='exclamation-circle' type={intl.formatMessage(messages.title)} />
-      <div className='error-column'>
-        <IconButton title={intl.formatMessage(messages.retry)} src={require('@tabler/icons/icons/refresh.svg')} onClick={handleRetry} />
-        {intl.formatMessage(messages.body)}
-      </div>
+    <Column label={intl.formatMessage(messages.title)}>
+      <Stack space={4} alignItems='center' justifyContent='center' className='bg-primary-50 dark:bg-slate-700 p-10 min-h-[160px] rounded-lg'>
+        <IconButton
+          iconClassName='w-20 h-20'
+          title={intl.formatMessage(messages.retry)}
+          src={require('@tabler/icons/icons/refresh.svg')}
+          onClick={handleRetry}
+        />
+
+        <Text align='center'>{intl.formatMessage(messages.body)}</Text>
+      </Stack>
     </Column>
   );
 };
