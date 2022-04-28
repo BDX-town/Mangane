527b5086a6302e23527b8c0df7ca6e1b5856c999
Alex Gleason <alex@alexgleason.me>
2022-04-28 16:33:42 -0500

    Counter: dark ring styles

--- app/soapbox/components/ui/counter/counter.tsx
+++ app/soapbox/components/ui/counter/counter.tsx
@@ -9,7 +9,7 @@ interface ICounter {
 /** A simple counter for notifications, etc. */
 const Counter: React.FC<ICounter> = ({ count }) => {
   return (
-    <span className='block px-1.5 py-0.5 bg-accent-500 text-xs text-white rounded-full ring-2 ring-white'>
+    <span className='block px-1.5 py-0.5 bg-accent-500 text-xs text-white rounded-full ring-2 ring-white dark:ring-slate-800'>
       {shortNumberFormat(count)}
     </span>
   );
