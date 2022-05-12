71e114d0492b91597ea4951e139a9f9a7f8b3b82
Alex Gleason <alex@alexgleason.me>
2022-05-11 20:47:21 -0500

    SiteLogo: fall back to Soapbox logo instead of Tabler's home.svg

--- app/soapbox/components/site-logo.tsx
+++ app/soapbox/components/site-logo.tsx
@@ -11,14 +11,19 @@ const SiteLogo: React.FC<React.ComponentProps<'img'>> = (props) => {
   const userTheme = settings.get('themeMode');
   const darkMode = userTheme === 'dark' || (userTheme === 'system' && systemTheme === 'dark');
 
+  /** Soapbox logo. */
+  const soapboxLogo = darkMode
+    ? require('images/soapbox-logo-white.svg')
+    : require('images/soapbox-logo.svg');
+
   // Use the right logo if provided, then use fallbacks.
   const getSrc = () => {
     // In demo mode, use the Soapbox logo.
-    if (settings.get('demo')) return require('images/soapbox-logo.svg');
+    if (settings.get('demo')) return soapboxLogo;
 
     return (darkMode && logoDarkMode)
       ? logoDarkMode
-      : logo || logoDarkMode || require('@tabler/icons/icons/home.svg');
+      : logo || logoDarkMode || soapboxLogo;
   };
 
   return (
