e6617af0f9d16d3df87f9482e0d32adec091e52a
Alex Gleason <alex@alexgleason.me>
2022-05-13 16:58:38 -0500

    Refactor context test, remove dumb fixtures

--- app/soapbox/reducers/__tests__/contexts-test.js
+++ app/soapbox/reducers/__tests__/contexts-test.js
@@ -4,9 +4,7 @@ import {
   fromJS,
 } from 'immutable';
 
-import context1 from 'soapbox/__fixtures__/context_1.json';
-import context2 from 'soapbox/__fixtures__/context_2.json';
-import { STATUSES_IMPORT } from 'soapbox/actions/importer';
+import { STATUS_IMPORT } from 'soapbox/actions/importer';
 import { CONTEXT_FETCH_SUCCESS } from 'soapbox/actions/statuses';
 import { TIMELINE_DELETE } from 'soapbox/actions/timelines';
 import { applyActions } from 'soapbox/jest/test-helpers';
@@ -21,59 +19,50 @@ describe('contexts reducer', () => {
     }));
   });
 
-  it('should support rendering a complete tree', () => {
-    // https://gitlab.com/soapbox-pub/soapbox-fe/-/issues/422
-    const actions = [{
-      type: STATUSES_IMPORT,
-      statuses: [
-        ...context1.ancestors,
-        ...context1.descendants,
-        ...context2.ancestors,
-        ...context2.descendants,
-      ],
-    }, {
-      type: CONTEXT_FETCH_SUCCESS,
-      id: '9zIH8WYwtnUx4yDzUm',
-      ancestors: context1.ancestors,
-      descendants: context1.descendants,
-    }, {
-      type: CONTEXT_FETCH_SUCCESS,
-      id: '9zIH7PUdhK3Ircg4hM',
-      ancestors: context2.ancestors,
-      descendants: context2.descendants,
-    }];
+  describe(CONTEXT_FETCH_SUCCESS, () => {
+    it('inserts a tombstone connecting an orphaned descendant', () => {
+      const status = { id: 'A', in_reply_to_id: null };
 
-    const result = applyActions(undefined, actions, reducer);
+      const context = {
+        id: 'A',
+        ancestors: [],
+        descendants: [
+          { id: 'C', in_reply_to_id: 'B' },
+        ],
+      };
 
-    expect(result).toEqual(ReducerRecord({
-      inReplyTos: ImmutableMap({
-        '9zIH7PUdhK3Ircg4hM': '9zIH6kDXA10YqhMKqO',
-        '9zIH7mMGgc1RmJwDLM': '9zIH6kDXA10YqhMKqO',
-        '9zIH9GTCDWEFSRt2um': '9zIH7PUdhK3Ircg4hM',
-        '9zIH9fhaP9atiJoOJc': '9zIH8WYwtnUx4yDzUm',
-        '9zIH8WYwtnUx4yDzUm': '9zIH7PUdhK3Ircg4hM',
-        '9zIH8WYwtnUx4yDzUm-tombstone': '9zIH7mMGgc1RmJwDLM',
-      }),
-      replies: ImmutableMap({
-        '9zIH6kDXA10YqhMKqO': ImmutableOrderedSet([
-          '9zIH7PUdhK3Ircg4hM',
-          '9zIH7mMGgc1RmJwDLM',
-        ]),
-        '9zIH7PUdhK3Ircg4hM': ImmutableOrderedSet([
-          '9zIH8WYwtnUx4yDzUm',
-          '9zIH9GTCDWEFSRt2um',
-        ]),
-        '9zIH8WYwtnUx4yDzUm': ImmutableOrderedSet([
-          '9zIH9fhaP9atiJoOJc',
-        ]),
-        '9zIH8WYwtnUx4yDzUm-tombstone': ImmutableOrderedSet([
-          '9zIH8WYwtnUx4yDzUm',
-        ]),
-        '9zIH7mMGgc1RmJwDLM': ImmutableOrderedSet([
-          '9zIH8WYwtnUx4yDzUm-tombstone',
-        ]),
-      }),
-    }));
+      const actions = [
+        { type: STATUS_IMPORT, status },
+        { type: CONTEXT_FETCH_SUCCESS, ...context },
+      ];
+
+      const result = applyActions(undefined, actions, reducer);
+      expect(result.inReplyTos.get('C')).toBe('C-tombstone');
+      expect(result.replies.get('A').toArray()).toEqual(['C-tombstone']);
+    });
+  });
+
+  describe(CONTEXT_FETCH_SUCCESS, () => {
+    it('inserts a tombstone connecting an orphaned descendant (with null in_reply_to_id)', () => {
+      const status = { id: 'A', in_reply_to_id: null };
+
+      const context = {
+        id: 'A',
+        ancestors: [],
+        descendants: [
+          { id: 'C', in_reply_to_id: null },
+        ],
+      };
+
+      const actions = [
+        { type: STATUS_IMPORT, status },
+        { type: CONTEXT_FETCH_SUCCESS, ...context },
+      ];
+
+      const result = applyActions(undefined, actions, reducer);
+      expect(result.inReplyTos.get('C')).toBe('C-tombstone');
+      expect(result.replies.get('A').toArray()).toEqual(['C-tombstone']);
+    });
   });
 
   describe(TIMELINE_DELETE, () => {
