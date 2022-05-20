d2968118518833d9da75a266539e0a1d497cdfba
Alex Gleason <alex@alexgleason.me>
2022-05-19 20:14:49 -0500

    Suggestions: store as OrderedSet

--- app/soapbox/reducers/suggestions.js
+++ app/soapbox/reducers/suggestions.js
@@ -1,4 +1,4 @@
-import { Map as ImmutableMap, List as ImmutableList, fromJS } from 'immutable';
+import { Map as ImmutableMap, OrderedSet as ImmutableOrderedSet, fromJS } from 'immutable';
 
 import { ACCOUNT_BLOCK_SUCCESS, ACCOUNT_MUTE_SUCCESS } from 'soapbox/actions/accounts';
 import { DOMAIN_BLOCK_SUCCESS } from 'soapbox/actions/domain_blocks';
@@ -14,7 +14,7 @@ import {
 } from '../actions/suggestions';
 
 const initialState = ImmutableMap({
-  items: ImmutableList(),
+  items: ImmutableOrderedSet(),
   next: null,
   isLoading: false,
 });
