131fcef7d63493940a43b1cdc68eba1388f1eda9
Alex Gleason <alex@alexgleason.me>
2022-06-04 15:51:59 -0500

    HomeTimeline: convert to TSX

---
+++ app/soapbox/features/home_timeline/index.tsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useRef } from 'react';
+import { defineMessages, useIntl, FormattedMessage } from 'react-intl';
+import { Link } from 'react-router-dom';
+
+import { expandHomeTimeline } from 'soapbox/actions/timelines';
+import { Column } from 'soapbox/components/ui';
+import Timeline from 'soapbox/features/ui/components/timeline';
+import { useAppSelector, useAppDispatch } from 'soapbox/hooks';
+
+const messages = defineMessages({
+  title: { id: 'column.home', defaultMessage: 'Home' },
+});
+
+const HomeTimeline: React.FC = () => {
+  const intl = useIntl();
+  const dispatch = useAppDispatch();
+  const polling = useRef<NodeJS.Timer | null>(null);
+
+  const isPartial = useAppSelector(state => state.timelines.getIn(['home', 'isPartial']) === true);
+  const siteTitle = useAppSelector(state => state.instance.title);
+
+  const handleLoadMore = (maxId: string) => {
+    dispatch(expandHomeTimeline({ maxId }));
+  };
+
+  useEffect(() => {
+    checkIfReloadNeeded();
+
+    return () => {
+      stopPolling();
+    };
+  }, [isPartial]);
+
+  // Mastodon generates the feed in Redis, and can return a partial timeline
+  // (HTTP 206) for new users. Poll until we get a full page of results.
+  const checkIfReloadNeeded = () => {
+    if (isPartial) {
+      polling.current = setInterval(() => {
+        dispatch(expandHomeTimeline());
+      }, 3000);
+    } else {
+      stopPolling();
+    }
+  };
+
+  const stopPolling = () => {
+    if (polling.current) {
+      clearInterval(polling.current);
+      polling.current = null;
+    }
+  };
+
+  const handleRefresh = () => {
+    return dispatch(expandHomeTimeline());
+  };
+
+  return (
+    <Column label={intl.formatMessage(messages.title)} transparent>
+      <Timeline
+        scrollKey='home_timeline'
+        onLoadMore={handleLoadMore}
+        onRefresh={handleRefresh}
+        timelineId='home'
+        divideType='space'
+        emptyMessage={<FormattedMessage id='empty_column.home' defaultMessage='Your home timeline is empty! Visit {public} to get started and meet other users.' values={{ public: <Link to='/timeline/local'><FormattedMessage id='empty_column.home.local_tab' defaultMessage='the {site_title} tab' values={{ site_title: siteTitle }} /></Link> }} />}
+      />
+    </Column>
+  );
+};
+
+export default HomeTimeline;
