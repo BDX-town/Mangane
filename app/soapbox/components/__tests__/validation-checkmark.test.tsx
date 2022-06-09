1b88f2f36e29a5622e23f9fc05ae8c4c5eff011b
Justin <dice.warrant-02@icloud.com>
2022-06-09 15:53:03 -0400

    Fix test

--- app/soapbox/components/__tests__/validation-checkmark.test.tsx
+++ app/soapbox/components/__tests__/validation-checkmark.test.tsx
@@ -16,14 +16,14 @@ describe('<ValidationCheckmark />', () => {
     render(<ValidationCheckmark text={text} isValid />);
 
     expect(screen.getByTestId('svg-icon-loader')).toHaveClass('text-success-500');
-    expect(screen.getByTestId('svg-icon-loader')).not.toHaveClass('text-gray-500');
+    expect(screen.getByTestId('svg-icon-loader')).not.toHaveClass('text-gray-400');
   });
 
   it('uses a gray check when valid', () => {
     const text = 'some validation';
     render(<ValidationCheckmark text={text} isValid={false} />);
 
-    expect(screen.getByTestId('svg-icon-loader')).toHaveClass('text-gray-500');
+    expect(screen.getByTestId('svg-icon-loader')).toHaveClass('text-gray-400');
     expect(screen.getByTestId('svg-icon-loader')).not.toHaveClass('text-success-500');
   });
 });
