fe9984dd9c91d3c9b301dc8e85c12a7285d0cb6b
Alex Gleason <alex@alexgleason.me>
2022-06-17 13:05:37 -0500

    ChatRoom: convert to TSX

---
+++ app/soapbox/features/chats/chat_room.tsx
@@ -0,0 +1,68 @@
+import { Map as ImmutableMap } from 'immutable';
+import React, { useEffect, useRef } from 'react';
+
+import { fetchChat, markChatRead } from 'soapbox/actions/chats';
+import { Column } from 'soapbox/components/ui';
+import { useAppSelector, useAppDispatch } from 'soapbox/hooks';
+import { makeGetChat } from 'soapbox/selectors';
+import { getAcct } from 'soapbox/utils/accounts';
+import { displayFqn as getDisplayFqn } from 'soapbox/utils/state';
+
+import ChatBox from './components/chat_box';
+
+const getChat = makeGetChat();
+
+interface IChatRoom {
+  params: {
+    chatId: string,
+  }
+}
+
+/** Fullscreen chat UI. */
+const ChatRoom: React.FC<IChatRoom> = ({ params }) => {
+  const dispatch = useAppDispatch();
+  const displayFqn = useAppSelector(getDisplayFqn);
+  const inputElem = useRef<HTMLInputElement | null>(null);
+
+  const chat = useAppSelector(state => {
+    const chat = state.chats.items.get(params.chatId, ImmutableMap()).toJS() as any;
+    return getChat(state, chat);
+  });
+
+  const focusInput = () => {
+    inputElem.current?.focus();
+  };
+
+  const handleInputRef = (el: HTMLInputElement) => {
+    inputElem.current = el;
+    focusInput();
+  };
+
+  const markRead = () => {
+    if (!chat) return;
+    dispatch(markChatRead(chat.id));
+  };
+
+  useEffect(() => {
+    dispatch(fetchChat(params.chatId));
+    markRead();
+  }, [params.chatId]);
+
+  // If this component is loaded at all, we can instantly mark new messages as read.
+  useEffect(() => {
+    markRead();
+  }, [chat?.unread]);
+
+  if (!chat) return null;
+
+  return (
+    <Column label={`@${getAcct(chat.account as any, displayFqn)}`}>
+      <ChatBox
+        chatId={chat.id}
+        onSetInputRef={handleInputRef}
+      />
+    </Column>
+  );
+};
+
+export default ChatRoom;
