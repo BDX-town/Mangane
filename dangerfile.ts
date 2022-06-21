504693db83cc82f3bf54bcc0c3e55e75a96c7057
Alex Gleason <alex@alexgleason.me>
2022-06-21 12:35:56 -0500

    Danger: fix test files glob

--- dangerfile.ts
+++ dangerfile.ts
@@ -2,7 +2,7 @@ import { danger, warn, message } from 'danger';
 
 const docs = danger.git.fileMatch('docs/**/*.md');
 const app = danger.git.fileMatch('app/**/*.(js|ts|tsx)');
-const tests = danger.git.fileMatch('*/__tests__/*');
+const tests = danger.git.fileMatch('**/__tests__/**');
 
 if (docs.edited) {
   message('Thanks - We :heart: our [documentarians](http://www.writethedocs.org/)!');
