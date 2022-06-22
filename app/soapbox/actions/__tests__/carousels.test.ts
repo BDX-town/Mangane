33bebf5bba7d3f47105cb79f5af6950520b83060
Justin <dice.warrant-02@icloud.com>
2022-06-22 08:55:42 -0400

    Add "carousel" redux logic

---
+++ app/soapbox/actions/__tests__/carousels.test.ts
@@ -0,0 +1,58 @@
+import { __stub } from 'soapbox/api';
+import { mockStore, rootState } from 'soapbox/jest/test-helpers';
+
+import { fetchCarouselAvatars } from '../carousels';
+
+describe('fetchCarouselAvatars()', () => {
+  let store;
+
+  beforeEach(() => {
+    store = mockStore(rootState);
+  });
+
+  describe('with a successful API request', () => {
+    let avatars;
+
+    beforeEach(() => {
+      avatars = [
+        { account_id: '1', username: 'jl', account_avatar: 'https://example.com/some.jpg' },
+      ];
+
+      __stub((mock) => {
+        mock.onGet('/api/v1/truth/carousels/avatars').reply(200, avatars);
+      });
+    });
+
+    it('should fetch the users from the API', async() => {
+      const expectedActions = [
+        { type: 'CAROUSEL_AVATAR_REQUEST' },
+        { type: 'CAROUSEL_AVATAR_SUCCESS', payload: avatars },
+      ];
+
+      await store.dispatch(fetchCarouselAvatars());
+      const actions = store.getActions();
+
+      expect(actions).toEqual(expectedActions);
+    });
+  });
+
+  describe('with an unsuccessful API request', () => {
+    beforeEach(() => {
+      __stub((mock) => {
+        mock.onGet('/api/v1/truth/carousels/avatars').networkError();
+      });
+    });
+
+    it('should dispatch failed action', async() => {
+      const expectedActions = [
+        { type: 'CAROUSEL_AVATAR_REQUEST' },
+        { type: 'CAROUSEL_AVATAR_FAIL' },
+      ];
+
+      await store.dispatch(fetchCarouselAvatars());
+      const actions = store.getActions();
+
+      expect(actions).toEqual(expectedActions);
+    });
+  });
+});
---
+++ app/soapbox/reducers/carousels.ts
@@ -0,0 +1,36 @@
+import { AnyAction } from 'redux';
+
+import {
+  CAROUSEL_AVATAR_REQUEST,
+  CAROUSEL_AVATAR_SUCCESS,
+  CAROUSEL_AVATAR_FAIL,
+} from '../actions/carousels';
+
+type Avatar = {
+  account_id: string
+  account_avatar: string
+  username: string
+}
+
+type CarouselsState = {
+  avatars: Avatar[],
+  isLoading: boolean
+}
+
+const initialState: CarouselsState = {
+  avatars: [],
+  isLoading: false,
+};
+
+export default function rules(state: CarouselsState = initialState, action: AnyAction): CarouselsState {
+  switch (action.type) {
+    case CAROUSEL_AVATAR_REQUEST:
+      return { ...state, isLoading: true };
+    case CAROUSEL_AVATAR_SUCCESS:
+      return { ...state, isLoading: false, avatars: action.payload };
+    case CAROUSEL_AVATAR_FAIL:
+      return { ...state, isLoading: false };
+    default:
+      return state;
+  }
+}
