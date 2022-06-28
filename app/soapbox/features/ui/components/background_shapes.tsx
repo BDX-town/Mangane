825f5566a01d996c201f16ec902ef680c1a5a303
Justin <dice.warrant-02@icloud.com>
2022-06-28 14:18:15 -0400

    Add CSS class with variables

--- app/soapbox/features/ui/components/background_shapes.tsx
+++ app/soapbox/features/ui/components/background_shapes.tsx
@@ -9,7 +9,7 @@ interface IBackgroundShapes {
 /** Gradient that appears in the background of the UI. */
 const BackgroundShapes: React.FC<IBackgroundShapes> = ({ position = 'fixed' }) => (
   <div className={classNames(position, 'top-0 inset-x-0 flex justify-center overflow-hidden pointer-events-none')}>
-    <div className='w-screen h-screen' style={{ background: 'linear-gradient(112deg, rgba(0,0,0,0) 0%, rgba(0,0,0,0) 20%, rgba(84,72,238,0.1) 30%, rgba(20,156,255,0.1) 70%, rgba(0,0,0,0) 80%, rgba(0,0,0,0) 100%)' }} />
+    <div className='w-screen h-screen bg-gradient' />
   </div>
 );
 
