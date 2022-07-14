21e7215bebf5926dacd84f8e0585b05098e0ec13
Alex Gleason <alex@alexgleason.me>
2022-07-13 19:09:13 -0500

    Remove legacy phone formatting code

--- app/soapbox/utils/phone.ts
+++ app/soapbox/utils/phone.ts
@@ -10,37 +10,8 @@ type CountryCode = typeof COUNTRY_CODES[number];
 /** Check whether a given value is a country code. */
 const isCountryCode = (value: any): value is CountryCode => COUNTRY_CODES.includes(value);
 
-function removeFormattingFromNumber(number = '') {
-  if (number) {
-    return number.toString().replace(/\D/g, '');
-  }
-
-  return number;
-}
-
-function formatPhoneNumber(countryCode: CountryCode, phoneNumber = '') {
-  let formattedPhoneNumber = '';
-  const strippedPhone = removeFormattingFromNumber(phoneNumber);
-
-  for (let i = 0; i < strippedPhone.length && i < 10; i++) {
-    const character = strippedPhone.charAt(i);
-    if (i === 0) {
-      const prefix = `+${countryCode} (`;
-      formattedPhoneNumber += prefix + character;
-    } else if (i === 3) {
-      formattedPhoneNumber += `) ${character}`;
-    } else if (i === 6) {
-      formattedPhoneNumber += `-${character}`;
-    } else {
-      formattedPhoneNumber += character;
-    }
-  }
-  return formattedPhoneNumber;
-}
-
 export {
   COUNTRY_CODES,
   CountryCode,
   isCountryCode,
-  formatPhoneNumber,
 };
