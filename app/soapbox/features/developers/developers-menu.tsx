cbd9dd52ee23bec3891c1b6457711dee228bd992
Alex Gleason <alex@alexgleason.me>
2022-07-21 19:46:14 -0500

    DevelopersMenu: refactor DashWidget into a React.FC

--- app/soapbox/features/developers/developers-menu.tsx
+++ app/soapbox/features/developers/developers-menu.tsx
@@ -15,7 +15,23 @@ const messages = defineMessages({
   leave: { id: 'developers.leave', defaultMessage: 'You have left developers' },
 });
 
-const Developers = () => {
+interface IDashWidget {
+  to?: string,
+  onClick?: React.MouseEventHandler<HTMLButtonElement>,
+  children: React.ReactNode,
+}
+
+const DashWidget: React.FC<IDashWidget> = ({ to, onClick, children }) => {
+  const className = 'bg-gray-200 dark:bg-gray-600 p-4 rounded flex flex-col items-center justify-center space-y-2 hover:-translate-y-1 transition-transform';
+
+  if (to) {
+    return <Link className={className} to={to}>{children}</Link>;
+  } else {
+    return <button className={className} onClick={onClick}>{children}</button>;
+  }
+};
+
+const Developers: React.FC = () => {
   const dispatch = useDispatch();
   const history = useHistory();
   const intl = useIntl();
@@ -31,53 +47,53 @@ const Developers = () => {
   return (
     <Column label={intl.formatMessage(messages.heading)}>
       <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2'>
-        <Link to='/developers/apps/create' className='bg-gray-200 dark:bg-gray-600 p-4 rounded flex flex-col items-center justify-center space-y-2 hover:-translate-y-1 transition-transform'>
+        <DashWidget to='/developers/apps/create'>
           <SvgIcon src={require('@tabler/icons/apps.svg')} className='dark:text-gray-100' />
 
           <Text>
             <FormattedMessage id='developers.navigation.app_create_label' defaultMessage='Create an app' />
           </Text>
-        </Link>
+        </DashWidget>
 
-        <Link to='/developers/settings_store' className='bg-gray-200 dark:bg-gray-600 p-4 rounded flex flex-col items-center justify-center space-y-2 hover:-translate-y-1 transition-transform'>
+        <DashWidget to='/developers/settings_store'>
           <SvgIcon src={require('@tabler/icons/code-plus.svg')} className='dark:text-gray-100' />
 
           <Text>
             <FormattedMessage id='developers.navigation.settings_store_label' defaultMessage='Settings store' />
           </Text>
-        </Link>
+        </DashWidget>
 
-        <Link to='/developers/timeline' className='bg-gray-200 dark:bg-gray-600 p-4 rounded flex flex-col items-center justify-center space-y-2 hover:-translate-y-1 transition-transform'>
+        <DashWidget to='/developers/timeline'>
           <SvgIcon src={require('@tabler/icons/home.svg')} className='dark:text-gray-100' />
 
           <Text>
             <FormattedMessage id='developers.navigation.test_timeline_label' defaultMessage='Test timeline' />
           </Text>
-        </Link>
+        </DashWidget>
 
-        <Link to='/error' className='bg-gray-200 dark:bg-gray-600 p-4 rounded flex flex-col items-center justify-center space-y-2 hover:-translate-y-1 transition-transform'>
+        <DashWidget to='/error'>
           <SvgIcon src={require('@tabler/icons/mood-sad.svg')} className='dark:text-gray-100' />
 
           <Text>
             <FormattedMessage id='developers.navigation.intentional_error_label' defaultMessage='Trigger an error' />
           </Text>
-        </Link>
+        </DashWidget>
 
-        <Link to='/error/network' className='bg-gray-200 dark:bg-gray-600 p-4 rounded flex flex-col items-center justify-center space-y-2 hover:-translate-y-1 transition-transform'>
+        <DashWidget to='/error/network'>
           <SvgIcon src={require('@tabler/icons/refresh.svg')} className='dark:text-gray-100' />
 
           <Text>
             <FormattedMessage id='developers.navigation.network_error_label' defaultMessage='Network error' />
           </Text>
-        </Link>
+        </DashWidget>
 
-        <button onClick={leaveDevelopers} className='bg-gray-200 dark:bg-gray-600 p-4 rounded flex flex-col items-center justify-center space-y-2 hover:-translate-y-1 transition-transform'>
+        <DashWidget onClick={leaveDevelopers}>
           <SvgIcon src={require('@tabler/icons/logout.svg')} className='dark:text-gray-100' />
 
           <Text>
             <FormattedMessage id='developers.navigation.leave_developers_label' defaultMessage='Leave developers' />
           </Text>
-        </button>
+        </DashWidget>
       </div>
     </Column>
   );
