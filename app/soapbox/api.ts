8d089805eaca80b728c8bc675ca65d34bc06fc05
Alex Gleason <alex@alexgleason.me>
2022-08-09 11:15:22 -0500

    Don't export getAuthBaseURL

--- app/soapbox/api.ts
+++ app/soapbox/api.ts
@@ -41,7 +41,7 @@ const maybeParseJSON = (data: string) => {
   }
 };
 
-export const getAuthBaseURL = createSelector([
+const getAuthBaseURL = createSelector([
   (state: RootState, me: string | false | null) => state.accounts.getIn([me, 'url']),
   (state: RootState, _me: string | false | null) => state.auth.get('me'),
 ], (accountUrl, authUserUrl) => {
