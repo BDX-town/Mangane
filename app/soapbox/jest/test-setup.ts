968ec3a7d2255444f8a38b1f84e92c67a8ef0bd8
Justin <dice.warrant-02@icloud.com>
2022-08-10 10:30:58 -0400

    Clear React Query cache before each test

--- app/soapbox/jest/test-setup.ts
+++ app/soapbox/jest/test-setup.ts
@@ -2,9 +2,14 @@
 
 import { __clear as clearApiMocks } from '../__mocks__/api';
 
+import { queryClient } from './test-helpers';
+
 // API mocking
 jest.mock('soapbox/api');
-afterEach(() => clearApiMocks());
+afterEach(() => {
+  clearApiMocks();
+  queryClient.clear();
+});
 
 // Mock IndexedDB
 // https://dev.to/andyhaskell/testing-your-indexeddb-code-with-jest-2o17
