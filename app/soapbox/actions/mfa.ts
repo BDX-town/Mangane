1c70fdaa52ee6f3ece00402af3119b654f25f8b3
uandreew <ihor@noleron.com>
2023-03-26 16:51:29 +0300

    fix /api/pleroma/accounts/mfa/

--- app/soapbox/actions/mfa.ts
+++ app/soapbox/actions/mfa.ts
@@ -26,7 +26,7 @@ const MFA_DISABLE_FAIL    = 'MFA_DISABLE_FAIL';
 const fetchMfa = () =>
   (dispatch: AppDispatch, getState: () => RootState) => {
     dispatch({ type: MFA_FETCH_REQUEST });
-    return api(getState).get('/api/v1/pleroma/accounts/mfa').then(({ data }) => {
+    return api(getState).get('/api/pleroma/accounts/mfa').then(({ data }) => {
       dispatch({ type: MFA_FETCH_SUCCESS, data });
     }).catch(() => {
       dispatch({ type: MFA_FETCH_FAIL });
@@ -36,7 +36,7 @@ const fetchMfa = () =>
 const fetchBackupCodes = () =>
   (dispatch: AppDispatch, getState: () => RootState) => {
     dispatch({ type: MFA_BACKUP_CODES_FETCH_REQUEST });
-    return api(getState).get('/api/v1/pleroma/accounts/mfa/backup_codes').then(({ data }) => {
+    return api(getState).get('/api/pleroma/accounts/mfa/backup_codes').then(({ data }) => {
       dispatch({ type: MFA_BACKUP_CODES_FETCH_SUCCESS, data });
       return data;
     }).catch(() => {
@@ -47,7 +47,7 @@ const fetchBackupCodes = () =>
 const setupMfa = (method: string) =>
   (dispatch: AppDispatch, getState: () => RootState) => {
     dispatch({ type: MFA_SETUP_REQUEST, method });
-    return api(getState).get(`/api/v1/pleroma/accounts/mfa/setup/${method}`).then(({ data }) => {
+    return api(getState).get(`/api/pleroma/accounts/mfa/setup/${method}`).then(({ data }) => {
       dispatch({ type: MFA_SETUP_SUCCESS, data });
       return data;
     }).catch((error: AxiosError) => {
@@ -60,7 +60,7 @@ const confirmMfa = (method: string, code: string, password: string) =>
   (dispatch: AppDispatch, getState: () => RootState) => {
     const params = { code, password };
     dispatch({ type: MFA_CONFIRM_REQUEST, method, code });
-    return api(getState).post(`/api/v1/pleroma/accounts/mfa/confirm/${method}`, params).then(({ data }) => {
+    return api(getState).post(`/api/pleroma/accounts/mfa/confirm/${method}`, params).then(({ data }) => {
       dispatch({ type: MFA_CONFIRM_SUCCESS, method, code });
       return data;
     }).catch((error: AxiosError) => {
@@ -72,7 +72,7 @@ const confirmMfa = (method: string, code: string, password: string) =>
 const disableMfa = (method: string, password: string) =>
   (dispatch: AppDispatch, getState: () => RootState) => {
     dispatch({ type: MFA_DISABLE_REQUEST, method });
-    return api(getState).delete(`/api/v1/pleroma/accounts/mfa/${method}`, { data: { password } }).then(({ data }) => {
+    return api(getState).delete(`/api/pleroma/accounts/mfa/${method}`, { data: { password } }).then(({ data }) => {
       dispatch({ type: MFA_DISABLE_SUCCESS, method });
       return data;
     }).catch((error: AxiosError) => {
