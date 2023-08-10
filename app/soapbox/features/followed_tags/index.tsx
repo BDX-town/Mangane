5a327b2baf08a2e85929e6e44cddeb0e72e47277
unknown <clovis@bdx.town>
2023-08-10 21:57:40 +0200

    Follow hashtag ok

--- app/soapbox/features/followed_tags/index.tsx
+++ app/soapbox/features/followed_tags/index.tsx
@@ -1,16 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { defineMessages, FormattedMessage, useIntl } from 'react-intl';
 
-import { fetchFilters, createFilter, deleteFilter } from 'soapbox/actions/filters';
-import snackbar from 'soapbox/actions/snackbar';
 import { followTag, unfollowTag } from 'soapbox/actions/tags';
 import Icon from 'soapbox/components/icon';
 import ScrollableList from 'soapbox/components/scrollable_list';
-import { Button, CardHeader, CardTitle, Column, Form, FormActions, FormGroup, Input, Spinner, Text } from 'soapbox/components/ui';
-import {
-  FieldsGroup,
-  Checkbox,
-} from 'soapbox/features/forms';
+import { Button, Column, IconButton, Spinner, Text } from 'soapbox/components/ui';
 import { useAppDispatch, useAppSelector } from 'soapbox/hooks';
 
 const messages = defineMessages({
@@ -22,7 +16,7 @@ interface IFollowButton {
   }
 
 const FollowButton: React.FC<IFollowButton> = ({ id }) => {
-  const { isFollow, loading } = useAppSelector(state => ({ loading: state.tags.loading, isFollow: state.tags.list.find((t) => t.name === id) }));
+  const { isFollow } = useAppSelector(state => ({ isFollow: state.tags.list.find((t) => t.name === id) }));
   const dispatch = useAppDispatch();
 
   const onClick = React.useCallback(() => {
@@ -31,26 +25,17 @@ const FollowButton: React.FC<IFollowButton> = ({ id }) => {
   }, [isFollow, id]);
 
   const text = React.useMemo(() => {
-    if (loading) return <FormattedMessage id='hashtag_timeline.loading' defaultMessage='Loading...' />;
     return isFollow ? (
       <FormattedMessage id='hashtag_timeline.unfollow' defaultMessage='Unfollow' />
 
     ) : (
       <FormattedMessage id='hashtag_timeline.follow' defaultMessage='Follow' />
     );
-  }, [loading, isFollow]);
+  }, [isFollow]);
 
 
   return (
-    <Button disabled={loading} theme='secondary' size='sm' onClick={onClick}>
-      {
-        loading ? <Spinner withText={false} size={16} /> : <Icon src={isFollow ? require('@tabler/icons/minus.svg') : require('@tabler/icons/plus.svg')} className='mr-1' />
-      }
-      &nbsp;
-      {
-        text
-      }
-    </Button>
+    <IconButton className='mx-3' style={{ background: 'transparent' }} onClick={onClick} src={isFollow ? require('@tabler/icons/minus.svg') : require('@tabler/icons/plus.svg')} text={text} />
   );
 };
 
@@ -74,21 +59,22 @@ const FollowedHashtags = () => {
           <Spinner />
         ) : (
           <ScrollableList
-            className='flex flex-col gap-3'
+            className='flex flex-col gap-2'
             scrollKey='followed_hashtags'
             emptyMessage={<FormattedMessage id='column.tags.empty' defaultMessage="You don't follow any hashtag yet." />}
           >
             {
               tags?.map((tag) => (
-                <div className='p-2 dark:bg-slate-900 rounded flex justify-between items-center'>
+                <div className='p-1 bg-gray-100 dark:bg-slate-900 rounded flex justify-between items-center'>
                   <div className='flex items-center'>
                     <Icon src={require('@tabler/icons/hash.svg')} />
                     <Text tag='span' weight='semibold'>
                       { tag.name }
                     </Text>
                   </div>
-                  <div className='flex items-center gap-2'>
+                  <div className='flex items-center gap-3'>
                     <FollowButton id={tag.name} />
+                    <span className='dark:text-slate-800 text-gray-300' >|</span>
                     <Button theme='link' to={`/tag/${tag.name}`}>
                       <div className='flex items-center'>
                         <FormattedMessage id='column.tags.see' defaultMessage='See' />
