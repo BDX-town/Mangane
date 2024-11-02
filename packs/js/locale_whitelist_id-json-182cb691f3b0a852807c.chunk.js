d96fca8d6b8fc88bd28b19e8eda54dde2b1934da
Cl0v1s <clovis@bdx.town>
2024-11-02 10:57:16 +0000

    Deploying to gh-pages from @ BDX-town/Mangane@f0fe8e651380ca6a2f1c4d6547f851462455f640 🚀

---
+++ packs/js/locale_whitelist_id-json-182cb691f3b0a852807c.chunk.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunkmangane_fe"] = self["webpackChunkmangane_fe"] || []).push([[169],{
+
+/***/ 1472:
+/*!***********************************************!*\
+  !*** ./app/soapbox/locales/whitelist_id.json ***!
+  \***********************************************/
+/***/ (function(module) {
+
+module.exports = [];
+
+/***/ })
+
+}]);
---
+++ packs/js/locale_whitelist_kk-json-5d268903b6cba3f70f70.chunk.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunkmangane_fe"] = self["webpackChunkmangane_fe"] || []).push([[175],{
+
+/***/ 1478:
+/*!***********************************************!*\
+  !*** ./app/soapbox/locales/whitelist_kk.json ***!
+  \***********************************************/
+/***/ (function(module) {
+
+module.exports = [];
+
+/***/ })
+
+}]);
