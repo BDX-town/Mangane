cfdb7912ffef695dd84d397caa0e206b2d47514f
Clovis <clovis@bdx.town>
2025-08-03 13:44:01 +0200

    chore: remove useless files

--- app/soapbox/utils/soapbox_prop_types.ts
+++
@@ -1,9 +0,0 @@
-import PropTypes from 'prop-types';
-
-export default {
-  me: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.oneOf([false, null]),
-  ]),
-  meLoggedIn: PropTypes.string,
-};
